4ed7e355adccb7850f0fd4b353c7d2718d136720
kempl <kempl95.kg@gmail.com>
2020-06-05 23:23:55 +0600

    #15 Изменение иерархии файла миграции после обновления

--- src/main/resources/db/migration/V1015__clear_patients_table.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM patients WHERE (`id` = '1');
-DELETE FROM patients WHERE (`id` = '2');
---
+++ src/main/resources/db/migration/V1017__clear_patients_table.sql
@@ -0,0 +1,7 @@
+DELETE FROM patients WHERE (`id` = '1');
+DELETE FROM patients WHERE (`id` = '2');
+
+DELETE FROM registration_places WHERE (`id` = '1');
+DELETE FROM registration_places WHERE (`id` = '2');
+DELETE FROM registration_places WHERE (`id` = '3');
+DELETE FROM registration_places WHERE (`id` = '4');
