1dfd5346bacf546fb80dd2552475a551923e1b4e
kempl <kempl95.kg@gmail.com>
2020-06-06 12:27:28 +0600

    #15 Для корректной работы фикстурных данных необходимо удалить данные, введенные через файлы миграции

---
+++ src/main/resources/db/migration/V1019__clear_insetrs_form_09.sql
@@ -0,0 +1,16 @@
+DELETE FROM registration_types WHERE (`id` = '1');
+DELETE FROM registration_types WHERE (`id` = '2');
+DELETE FROM registration_types WHERE (`id` = '3');
+DELETE FROM registration_types WHERE (`id` = '4');
+
+DELETE FROM positions WHERE (`id` = '1');
+DELETE FROM positions WHERE (`id` = '2');
+DELETE FROM positions WHERE (`id` = '3');
+DELETE FROM positions WHERE (`id` = '4');
+DELETE FROM positions WHERE (`id` = '5');
+
+DELETE FROM hospitals WHERE (`id` = '1');
+DELETE FROM hospitals WHERE (`id` = '2');
+DELETE FROM hospitals WHERE (`id` = '3');
+DELETE FROM hospitals WHERE (`id` = '4');
+
