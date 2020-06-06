ad8918cd6ea99efde1e2a21f0342805e3059e2fa
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-06-06 19:49:10 +0600

    18 Просмотр записей у врача и пациента

--- src/main/java/com/project/crm/backend/repository/JournalRepo.java
+++ src/main/java/com/project/crm/backend/repository/JournalRepo.java
@@ -1,7 +1,14 @@
 package com.project.crm.backend.repository;
 
+import com.project.crm.backend.model.Doctor;
+import com.project.crm.backend.model.Patient;
 import com.project.crm.backend.model.catalog.Journal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface JournalRepo extends JpaRepository<Journal, Long> {
+    Optional<Journal> findByDoctor(Doctor doctor);
+    Optional<Journal> findByPatient(Patient patient);
 }
