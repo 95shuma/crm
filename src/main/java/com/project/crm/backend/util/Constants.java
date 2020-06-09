91b31f2f0e3bf51a71e5f29b4d12b5b62bc9ad44
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-06-09 14:57:16 +0600

    #29 изменил название констант

--- src/main/java/com/project/crm/backend/util/Constants.java
+++ src/main/java/com/project/crm/backend/util/Constants.java
@@ -1,16 +1,12 @@
 package com.project.crm.backend.util;
 
-import com.project.crm.backend.model.catalog.Role;
-import com.project.crm.backend.repository.RoleRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-
 public final class Constants {
 
-    public static final String ADMIN_ROLE_ID = "1" ;
-    public static final String ADMIN_HCF_ROLE_ID = "3";
-    public static final String JMO_ROLE_ID = "4";
-    public static final String DOCTOR_ROLE_ID = "2";
-    public static final String PATIENT_ROLE_ID = "5";
+    public static final String ADMIN = "cупер админ" ;
+    public static final String ADMIN_HCF = "админ ЛПУ";
+    public static final String DOCTOR = "доктор";
+    public static final String JMO = "млад мед персонал";
+    public static final String PATIENT = "пациент";
 
 
 
