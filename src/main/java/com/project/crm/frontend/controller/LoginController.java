842a587c7cc8364fa0b4088fe7d669e22c9dab78
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-06-10 19:09:52 +0600

    #30 создал логин контроллер

--- src/main/java/com/project/crm/frontend/controller/LoginController.java
+++ src/main/java/com/project/crm/frontend/controller/LoginController.java
@@ -1,47 +1,50 @@
-//package com.project.crm.frontend.controller;
-//
-//import com.project.crm.backend.services.AdministratorService;
-//import com.project.crm.backend.services.HospitalsDoctorService;
-//import com.project.crm.backend.services.UserService;
-//import lombok.AllArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//import java.security.Principal;
-//
-//@Controller
-//@RequestMapping
-//@AllArgsConstructor
-//public class LoginController {
-//
-//    private final AdministratorService administratorService;
-//    private final HospitalsDoctorService hospitalsDoctorService;
-//    private final UserService userService;
-//
-//    @GetMapping("/login")
-//    public String loginPage(Model model, @RequestParam(required = false, defaultValue = "false") Boolean error, Principal principal) {
-//
-//        userService.checkUserPresence(model, principal);
-//
-//        model.addAttribute("error", error);
-//        return "login";
-//    }
-//    @GetMapping("/default")
-//    public String defaultPage(@RequestParam(required = false, defaultValue = "false") Boolean error, Model model, Principal principal) {
-//        model.addAttribute("error", error);
-//        if(principal != null){
-//            model.addAttribute("isLoggedIn", true);
-//            if (administratorService.existByInn(principal.getName())) {
-//                return "redirect:/admin";
-//            } else if (hospitalsDoctorService.existByInnForAdminHCF(principal.getName())){
-//                return "redirect:/adminHCF";
-//            } else if (hospitalsDoctorService.existByInnForDoctor(principal.getName())){
-//                return "redirect:/doctor";
-//            } else {
-//                return "redirect:/patient";
-//            }
-//        } else {
-//            return "redirect:/login";
-//        }
-//    }
-//}
+package com.project.crm.frontend.controller;
+import com.project.crm.backend.services.RegistrationJournalService;
+import com.project.crm.backend.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.security.Principal;
+
+@Controller
+@RequestMapping
+@AllArgsConstructor
+public class LoginController {
+
+    private final UserService userService;
+    private final RegistrationJournalService registrationJournalService;
+
+    @GetMapping("/login")
+    public String login(Model model, @RequestParam(required = false, defaultValue = "false") Boolean error, Principal principal) {
+
+        userService.checkUserPresence(model, principal);
+
+        model.addAttribute("error", error);
+        return "login";
+    }
+
+    @GetMapping("/default")
+    public String defaultPage(@RequestParam(required = false, defaultValue = "false") Boolean error, Model model, Principal principal) {
+
+        model.addAttribute("error", error);
+
+        if(principal != null){
+
+            String inn = principal.getName();
+
+            if (registrationJournalService.existsByUserInnAndRoleId(inn, (long) 1)) {
+                return "redirect:/admin";
+            } else if (registrationJournalService.existsByUserInnAndRoleId(inn, (long) 2)){
+                return "redirect:/senior-doctor";
+            } else if (registrationJournalService.existsByUserInnAndRoleId(inn, (long) 3)){
+                return "redirect:/doctor";
+            } else if (registrationJournalService.existsByUserInnAndRoleId(inn, (long) 4)){
+//                return "redirect:/junior-doctor";
+            } else if (registrationJournalService.existsByUserInnAndRoleId(inn, (long) 5)){
+                return "redirect:/patient";
+            }
+        }
+        return "/login";
+    }
+}
