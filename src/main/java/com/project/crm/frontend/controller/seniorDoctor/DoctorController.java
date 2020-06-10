f28863ab3742037dcb421b29d9e3c3d546c4eda9
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-06-10 19:09:15 +0600

    #30 создал папку для панели админу лпу и добавил соответствующие контроллеры

---
+++ src/main/java/com/project/crm/frontend/controller/seniorDoctor/DoctorController.java
@@ -0,0 +1,60 @@
+package com.project.crm.frontend.controller.seniorDoctor;
+
+import com.project.crm.backend.services.*;
+import com.project.crm.frontend.forms.UserRegisterForm;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller("pkg seniorDoctor Doctor")
+@RequestMapping("/senior-doctor/doctors")
+@AllArgsConstructor
+public class DoctorController {
+
+    private final UserService userService;
+    private final HospitalService hospitalService;
+    private final RoleService roleService;
+    private final PositionService positionService;
+    private final PlaceService placeService;
+
+    @GetMapping("/doctor")
+    public String doctorRegisterPage(Model model, Principal principal){
+
+        userService.checkUserPresence(model, principal);
+
+        if (!model.containsAttribute("reg")) {
+            model.addAttribute("reg", new UserRegisterForm());
+        }
+        model.addAttribute("registrationPlaces", placeService.getAll());
+        model.addAttribute("hospitals", hospitalService.getAll());
+        model.addAttribute("roles", roleService.getAll());
+        model.addAttribute("positions", positionService.getAll());
+        return "doctorRegister";
+    }
+
+
+
+    @PostMapping
+    public String doctorRegister(@Valid UserRegisterForm userRegisterForm,
+                                 BindingResult validationResult,
+                                 RedirectAttributes attributes){
+        attributes.addFlashAttribute("reg", userRegisterForm);
+
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/senior-doctor/doctors/doctor";
+        }
+
+        userService.createUser(userRegisterForm);
+
+        return "redirect:/senior-doctor";
+    }
+}
---
+++ src/main/java/com/project/crm/frontend/controller/seniorDoctor/PatientController.java
@@ -0,0 +1,57 @@
+package com.project.crm.frontend.controller.seniorDoctor;
+
+import com.project.crm.backend.services.HospitalService;
+import com.project.crm.backend.services.PlaceService;
+import com.project.crm.backend.services.UserService;
+import com.project.crm.frontend.forms.UserRegisterForm;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.security.Principal;
+
+@Controller("pkg seniorDoctor Patient")
+@RequestMapping("/senior-doctor/patients")
+@AllArgsConstructor
+public class PatientController {
+
+    private final UserService userService;
+    private final HospitalService hospitalService;
+    private final PlaceService placeService;
+
+    @GetMapping("/patient")
+    public String getPatient(Model model, Principal principal){
+
+        userService.checkUserPresence(model, principal);
+
+        if (!model.containsAttribute("reg")) {
+            model.addAttribute("reg", new UserRegisterForm());
+        }
+        model.addAttribute("hospitals", hospitalService.getAll());
+        model.addAttribute("registrationPlaces", placeService.getAll());
+        return "patientRegister";
+    }
+
+    @PostMapping
+    public String createPatient(@Valid UserRegisterForm userRegisterForm,
+                                  BindingResult validationResult,
+                                  RedirectAttributes attributes){
+
+        attributes.addFlashAttribute("reg", userRegisterForm);
+
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/senior-doctor/patients/patient";
+        }
+
+        userService.createUser(userRegisterForm);
+
+        return "redirect:/senior-doctor";
+    }
+}
