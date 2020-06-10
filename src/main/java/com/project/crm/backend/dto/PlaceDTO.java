fa8e48a121d8deec7857b6b1a95963a708ffa228
Baktygul <bakulyakozhoshova@gmail.com>
2020-06-10 22:40:41 +0600

    #38 добавлен PlaceDTO

---
+++ src/main/java/com/project/crm/backend/dto/PlaceDTO.java
@@ -0,0 +1,26 @@
+package com.project.crm.backend.dto;
+
+import com.project.crm.backend.model.catalog.Place;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PlaceDTO {
+    private Long id;
+    private String name;
+    private String codePlace;
+    private Integer groupCode;
+
+    static PlaceDTO from(Place place) {
+        return builder()
+                .id(place.getId())
+                .name(place.getName())
+                .codePlace(place.getCodePlace())
+                .groupCode(place.getGroupCode())
+                .build();
+    }
+}
