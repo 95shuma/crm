e172dd485719745f68ac8b924780494ba8909477
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-06-10 19:12:31 +0600

    #30 добавил новый сервис

---
+++ src/main/java/com/project/crm/backend/services/PropertiesService.java
@@ -0,0 +1,41 @@
+package com.project.crm.backend.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+@Service
+@AllArgsConstructor
+public class PropertiesService {
+    private final SpringDataWebProperties pageableDefaultProps;
+
+    public int getDefaultPageSize() {
+        return pageableDefaultProps.getPageable().getDefaultPageSize();
+    }
+
+    public void setDefaultPageSize(int pageSize){
+        pageableDefaultProps.getPageable().setDefaultPageSize(pageSize);
+    }
+
+    public static <T> void constructPageable(Page<T> list, int pageSize, Model model, String uri) {
+        if (list.hasNext()) {
+            model.addAttribute("nextPageLink", constructPageUri(uri, list.nextPageable().getPageNumber(), list.nextPageable().getPageSize()));
+        }
+
+        if (list.hasPrevious()) {
+            model.addAttribute("prevPageLink", constructPageUri(uri, list.previousPageable().getPageNumber(), list.previousPageable().getPageSize()));
+        }
+
+        model.addAttribute("hasNext", list.hasNext());
+        model.addAttribute("hasPrev", list.hasPrevious());
+        model.addAttribute("items", list.getContent());
+        model.addAttribute("defaultPageSize", pageSize);
+    }
+
+    private static String constructPageUri(String uri, int page, int size) {
+        return String.format("%s?page=%s&size=%s", uri, page, size);
+    }
+
+}
