deeefe6d94b1246f7e1103f00291c9334ee0ab8c
Baktygul <bakulyakozhoshova@gmail.com>
2020-06-10 23:34:03 +0600

    #38 добавлен RegistrationJournalDTO

---
+++ src/main/java/com/project/crm/backend/dto/RegistrationJournalDTO.java
@@ -0,0 +1,29 @@
+package com.project.crm.backend.dto;
+
+
+import com.project.crm.backend.model.catalog.RegistrationJournal;
+import lombok.*;
+
+
+@Data
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class RegistrationJournalDTO {
+    private Long id;
+    private HospitalDTO hospital;
+    private UserDTO user;
+    private PositionDTO position;
+    private RoleDTO role;
+
+
+    static RegistrationJournalDTO from(RegistrationJournal registrationJournal) {
+        return builder()
+                .id(registrationJournal.getId())
+                .hospital(HospitalDTO.from(registrationJournal.getHospital()))
+                .user(UserDTO.from(registrationJournal.getUser()))
+                .position(PositionDTO.from(registrationJournal.getPosition()))
+                .role(RoleDTO.from(registrationJournal.getRole()))
+                .build();
+    }
+}
