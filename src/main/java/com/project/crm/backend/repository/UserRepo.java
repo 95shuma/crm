ade7b99dcc84e29f9ebf6b0b97b9ca5857a1d3b5
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-06-11 22:37:30 +0600

    #35 добавил новые методы

--- src/main/java/com/project/crm/backend/repository/UserRepo.java
+++ src/main/java/com/project/crm/backend/repository/UserRepo.java
@@ -2,7 +2,9 @@ package com.project.crm.backend.repository;
 
 import com.project.crm.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepo extends JpaRepository<User, Long> {
@@ -10,4 +12,9 @@ public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByInn(String inn);
     Optional<User> findByName(String name);
     boolean existsByInn(String inn);
+    @Query(value = "select u.* from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id = 5", nativeQuery = true)
+    List<User> findAllPatients();
+
+    @Query(value = "select u.* from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id != 5 and rj.role_id != 1", nativeQuery = true)
+    List<User> findAllHospitalStaff();
 }
