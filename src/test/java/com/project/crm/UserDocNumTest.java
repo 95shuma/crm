893bf38ba3c521744613a852c304505df4474517
95shuma <95shuma@gmail.com>
2020-06-14 16:54:13 +0600

    #44 вытащил рандомного пользователя

---
+++ src/test/java/com/project/crm/UserDocNumTest.java
@@ -0,0 +1,23 @@
+package com.project.crm;
+
+import com.project.crm.backend.services.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+@SpringBootTest
+public class UserDocNumTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void test_method_for_place_uniqueness2(){
+        Random random = new Random();
+        var userForTest = userService.getAll().get(random.nextInt(userService.getAll().size()));
+    }
+}
