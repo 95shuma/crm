f8dbefe11f2c9265bf8cd39c1400ccc27e44a3d8
95shuma <95shuma@gmail.com>
2020-06-17 05:05:28 +0600

    #47 редактировал аннотации в контроллере доктор

--- src/main/java/com/project/crm/frontend/controller/admin/DoctorController.java
+++ src/main/java/com/project/crm/frontend/controller/admin/DoctorController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
-@Controller("pkg seniorDoctor Doctor")
+@Controller("pkg admin Doctor")
 @RequestMapping("/admin/doctors")
 @AllArgsConstructor
 public class DoctorController {
