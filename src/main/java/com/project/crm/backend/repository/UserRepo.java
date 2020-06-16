ed9e26b4341c698180a8de2e80ea43f2914eb8d9
kempl <kempl95.kg@gmail.com>
2020-06-16 21:23:43 +0600

    #51 изменил имена методов на более корректные. Также изменил параметр запроса

--- src/main/java/com/project/crm/backend/repository/UserRepo.java
+++ src/main/java/com/project/crm/backend/repository/UserRepo.java
@@ -14,12 +14,15 @@ public interface UserRepo extends JpaRepository<User, Long> {
     Optional<User> findByInn(String inn);
     Optional<User> findByName(String name);
     boolean existsByInn(String inn);
-    @Query(value = "select u.* from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id = 5", nativeQuery = true)
+    @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id = 5", nativeQuery = true)
     List<User> findAllPatients();
+
+    @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id = 5", nativeQuery = true)
+    List<User> findAllPatients(Pageable pageable);
 
     @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id != 5 and rj.role_id != 1", nativeQuery = true)
     List<User> findAllHospitalStaff();
 
     @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id != 5 and rj.role_id != 1", nativeQuery = true)
-    Page<User> findAllHospitalStaffPageable(Pageable pageable);
+    Page<User> findAllHospitalStaff(Pageable pageable);
 }
