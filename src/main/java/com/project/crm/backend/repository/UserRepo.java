af5cc591b4af551d5b8338a83bd3b7ddf18d8c51
kempl <kempl95.kg@gmail.com>
2020-06-16 19:11:52 +0600

    #51 добавил запрос, возвращающий Page <List> докторов

--- src/main/java/com/project/crm/backend/repository/UserRepo.java
+++ src/main/java/com/project/crm/backend/repository/UserRepo.java
@@ -1,6 +1,8 @@
 package com.project.crm.backend.repository;
 
 import com.project.crm.backend.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -17,4 +19,7 @@ public interface UserRepo extends JpaRepository<User, Long> {
 
     @Query(value = "select u.* from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id != 5 and rj.role_id != 1", nativeQuery = true)
     List<User> findAllHospitalStaff();
+
+    @Query(value = "select u.* from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id != 5 and rj.role_id != 1", nativeQuery = true)
+    Page<User> findAllHospitalStaffPageable(Pageable pageable);
 }
