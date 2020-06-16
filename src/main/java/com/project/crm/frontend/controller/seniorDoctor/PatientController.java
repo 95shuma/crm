220f5c374a1b95177b90cba7eeb81e5c0edadb9a
kempl <kempl95.kg@gmail.com>
2020-06-16 21:33:54 +0600

    #51 Список пациентов возвращается в постраничном виде

--- src/main/java/com/project/crm/frontend/controller/seniorDoctor/PatientController.java
+++ src/main/java/com/project/crm/frontend/controller/seniorDoctor/PatientController.java
@@ -2,9 +2,11 @@ package com.project.crm.frontend.controller.seniorDoctor;
 
 import com.project.crm.backend.services.HospitalService;
 import com.project.crm.backend.services.PlaceService;
+import com.project.crm.backend.services.PropertiesService;
 import com.project.crm.backend.services.UserService;
 import com.project.crm.frontend.forms.UserRegisterForm;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.security.Principal;
 
@@ -24,12 +27,11 @@ public class PatientController {
     private final UserService userService;
     private final HospitalService hospitalService;
     private final PlaceService placeService;
+    private final PropertiesService propertiesService;
 
     @GetMapping("/patient")
     public String getPatient(Model model, Principal principal){
-
         userService.checkUserPresence(model, principal);
-
         if (!model.containsAttribute("reg")) {
             model.addAttribute("reg", new UserRegisterForm());
         }
@@ -37,29 +39,22 @@ public class PatientController {
         model.addAttribute("places", placeService.getAll());
         return "seniorDoctor/patientController/patientRegister";
     }
-
-
     @GetMapping
-    public String getPatients(Model model, Principal principal){
+    public String getPatients(Model model, Pageable pageable, HttpServletRequest uriBuilder, Principal principal){
         userService.checkUserPresence(model, principal);
-        model.addAttribute("patients", userService.getAllPatients());
+        PropertiesService.constructPageable(userService.getAllPatients(pageable), propertiesService.getDefaultPageSize(), model, uriBuilder.getRequestURI());
         return "seniorDoctor/patientController/patients";
     }
-
     @PostMapping("/patient")
     public String createPatient(@Valid UserRegisterForm userRegisterForm,
                                   BindingResult validationResult,
                                   RedirectAttributes attributes){
-
         attributes.addFlashAttribute("reg", userRegisterForm);
-
         if (validationResult.hasFieldErrors()) {
             attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
             return "redirect:/senior-doctor/patients/patient";
         }
-
         userService.createUser(userRegisterForm);
-
         return "redirect:/senior-doctor";
     }
 }
