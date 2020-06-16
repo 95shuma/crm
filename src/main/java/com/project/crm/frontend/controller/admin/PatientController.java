009bf9a1c374b8f4d605da7325c4fb060644a759
95shuma <95shuma@gmail.com>
2020-06-17 04:50:40 +0600

    #47 добавил контроллер для отображения всех пациентов

---
+++ src/main/java/com/project/crm/frontend/controller/admin/PatientController.java
@@ -0,0 +1,29 @@
+package com.project.crm.frontend.controller.admin;
+
+import com.project.crm.backend.services.PropertiesService;
+import com.project.crm.backend.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@Controller("pkg seniorDoctor Patient")
+@RequestMapping("/admin/patients")
+@AllArgsConstructor
+public class PatientController {
+
+    private final UserService userService;
+    private final PropertiesService propertiesService;
+
+    @GetMapping
+    public String getPatients(Model model, Pageable pageable, HttpServletRequest uriBuilder, Principal principal){
+        userService.checkUserPresence(model, principal);
+        PropertiesService.constructPageable(userService.getAllPatients(pageable), propertiesService.getDefaultPageSize(), model, uriBuilder.getRequestURI());
+        return "seniorDoctor/patientController/patients";
+    }
+}
