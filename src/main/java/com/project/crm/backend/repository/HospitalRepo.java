1bcd551dd5371276fba345b237964f864e628d51
95shuma <95shuma@gmail.com>
2020-06-17 06:07:48 +0600

    #47 добавил метод для вывода из базы всех ЛПУ

--- src/main/java/com/project/crm/backend/repository/HospitalRepo.java
+++ src/main/java/com/project/crm/backend/repository/HospitalRepo.java
@@ -1,13 +1,15 @@
 package com.project.crm.backend.repository;
 
 import com.project.crm.backend.model.catalog.Hospital;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 public interface HospitalRepo extends JpaRepository<Hospital, Long> {
 
     Optional<Hospital> findByName(String name);
+
+    Page<Hospital> findAll(Pageable page);
 }
