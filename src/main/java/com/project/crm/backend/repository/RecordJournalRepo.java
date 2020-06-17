71653f15b62c411d1e50adef3cffe5e0672fc289
95shuma <95shuma@gmail.com>
2020-06-18 05:57:55 +0600

    #57 добавил сортировку для просмотра записей по дате

--- src/main/java/com/project/crm/backend/repository/RecordJournalRepo.java
+++ src/main/java/com/project/crm/backend/repository/RecordJournalRepo.java
@@ -4,6 +4,7 @@ import com.project.crm.backend.model.catalog.RecordJournal;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,5 +12,7 @@ public interface RecordJournalRepo extends JpaRepository<RecordJournal, Long> {
 
     List<RecordJournal> findByDoctorId(Long id);
     List<RecordJournal> findByPatientId(Long id);
+
+    @Query(value = "select * from records_journal as rj where rj.doctor_id = ?1 order by rj.date_time asc", nativeQuery = true)
     Page<RecordJournal> findAllByDoctorId(Long id, Pageable pageable);
 }
