b9e0326943e2190939c375a06131f3bfb59e380b
95shuma <95shuma@gmail.com>
2020-06-18 05:56:57 +0600

    #57 добавил сортировку для списка ЛПУ по названию

--- src/main/java/com/project/crm/backend/repository/HospitalRepo.java
+++ src/main/java/com/project/crm/backend/repository/HospitalRepo.java
@@ -4,6 +4,7 @@ import com.project.crm.backend.model.catalog.Hospital;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -11,5 +12,6 @@ public interface HospitalRepo extends JpaRepository<Hospital, Long> {
 
     Optional<Hospital> findByName(String name);
 
+    @Query(value = "select * from hospitals as h order by h.name asc", nativeQuery = true)
     Page<Hospital> findAll(Pageable page);
 }
