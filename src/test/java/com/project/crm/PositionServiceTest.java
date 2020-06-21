7cbe4ab4c9d3b959152d8b94c8f4a2562fbe3394
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-06-21 13:08:30 +0600

    #64 создал тест на проверку функциональности валидации в поле name в обьекте PositionDTO

---
+++ src/test/java/com/project/crm/PositionServiceTest.java
@@ -0,0 +1,41 @@
+package com.project.crm;
+
+import com.project.crm.backend.dto.PositionDTO;
+import com.project.crm.backend.services.PositionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class PositionServiceTest {
+
+    @Autowired
+    private PositionService positionService;
+
+    @Test
+    public void invalidPositionNameShouldFailValidation(){
+        PositionDTO positionDTO = PositionDTO.builder()
+                .name(null)
+                .build();
+
+        assertThrows(ConstraintViolationException.class, () -> {
+            positionService.createPosition(positionDTO);
+        });
+    }
+    @Test
+    public void validPositionNameShouldPassValidation(){
+        PositionDTO positionDTO = PositionDTO.builder()
+                .name("name")
+                .build();
+
+        assertEquals("name",
+            positionService.createPosition(positionDTO).getName());
+    }
+
+}
