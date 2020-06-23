33020e46a58ceeaccc00755f04083a400096b12a
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-06-24 02:33:13 +0600

    #63 Добавил нужные аннотации(9)

--- src/main/java/com/project/crm/frontend/forms/RecordJournalRegisterForm.java
+++ src/main/java/com/project/crm/frontend/forms/RecordJournalRegisterForm.java
@@ -2,17 +2,26 @@ package com.project.crm.frontend.forms;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Getter
 @Setter
 public class RecordJournalRegisterForm {
 
+    @NotNull(message = "NotNull - doctor")
     private Long doctorId = null;
 
     private Long registrarId = null;
 
+    @Future(message = "Нельзя записаться в прошлое")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    private LocalDateTime dateTime;
+
+    @NotNull(message = "NotNull - hospital")
     private Long hospitalId = null;
 
     @NotBlank(message = "Укажите причину")
