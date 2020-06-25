3018684d0ddf7217f5ece032efbbb0c251f30432
Baktygul <bakulyakozhoshova@gmail.com>
2020-06-26 00:42:46 +0600

    #75 добавила RemedyTypeService и метод в репо

--- src/main/java/com/project/crm/backend/repository/RemedyTypeRepo.java
+++ src/main/java/com/project/crm/backend/repository/RemedyTypeRepo.java
@@ -1,9 +1,12 @@
 package com.project.crm.backend.repository;
 
 
+import com.project.crm.backend.model.catalog.Position;
 import com.project.crm.backend.model.catalog.remediesCatalog.RemedyType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RemedyTypeRepo extends JpaRepository<RemedyType, Long> {
+import java.util.Optional;
 
+public interface RemedyTypeRepo extends JpaRepository<RemedyType, Long> {
+    Optional<RemedyType> findByName(String name);
 }
