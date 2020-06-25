f192f41a768b4a16a35b00dc0053080a1c4cd8d9
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-06-25 17:34:38 +0600

    #71 добавил два новых обьекта: PositionFormValidatorTest, RecordJournalFormValidatorTest

---
+++ src/test/java/com/project/crm/PositionFormValidatorTest.java
@@ -0,0 +1,22 @@
+package com.project.crm;
+
+import com.project.crm.frontend.forms.PositionRegisterForm;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class PositionFormValidatorTest {
+
+
+}
---
+++ src/test/java/com/project/crm/RecordJournalFormValidatorTest.java
@@ -0,0 +1,26 @@
+package com.project.crm;
+
+import com.project.crm.frontend.forms.RecordJournalRegisterForm;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class RecordJournalFormValidatorTest {
+
+
+}
