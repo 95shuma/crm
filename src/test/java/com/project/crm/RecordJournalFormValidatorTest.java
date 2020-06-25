84c603cd41c72e022220dffe652309d842a8fe77
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-06-25 17:36:58 +0600

    #71 добавил тесты на проверку аннотации на поле reason, datetime

--- src/test/java/com/project/crm/RecordJournalFormValidatorTest.java
+++ src/test/java/com/project/crm/RecordJournalFormValidatorTest.java
@@ -22,5 +22,59 @@ import static org.junit.Assert.*;
 @SpringBootTest
 public class RecordJournalFormValidatorTest {
 
+    @Autowired
+    private Validator validator;
 
+    private RecordJournalRegisterForm recordJournalRegisterForm;
+
+    @Before
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Before
+    public void setUpRecordJournalRegisterForm(){
+        recordJournalRegisterForm = new RecordJournalRegisterForm();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        recordJournalRegisterForm = null;
+    }
+
+    @Test
+    public void invalidRecordJournalFormShouldFailValidation() {
+
+
+        recordJournalRegisterForm.setReason("");
+        recordJournalRegisterForm.setDateTime(LocalDateTime.now().minusDays(1));
+
+        Set<ConstraintViolation<RecordJournalRegisterForm>> violations = validator.validate(recordJournalRegisterForm);
+        List<String> errorMessages = violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList());
+
+        assertEquals(2, violations.size());
+        assertTrue(errorMessages.containsAll(Arrays.asList("Укажите причину", "Нельзя записаться в прошлое")));
+        assertFalse(violations.isEmpty());
+    }
+    @Test
+    public void invalidRecordJournalFormDateShouldFailValidation() {
+
+
+        recordJournalRegisterForm.setReason("something");
+
+        Set<ConstraintViolation<RecordJournalRegisterForm>> violations = validator.validate(recordJournalRegisterForm);
+        List<String> errorMessages = violations.stream().map(ConstraintViolation::getMessage).collect(Collectors.toList());
+
+        assertEquals(1, violations.size());
+        assertFalse(violations.isEmpty());
+    }
+    @Test
+    public void validPositionNameShouldPassValidation() {
+
+        recordJournalRegisterForm.setReason("something");
+        recordJournalRegisterForm.setDateTime(LocalDateTime.now().plusDays(1));
+        Set<ConstraintViolation<RecordJournalRegisterForm>> violations = validator.validate(recordJournalRegisterForm);
+        assertTrue(violations.isEmpty());
+    }
 }
