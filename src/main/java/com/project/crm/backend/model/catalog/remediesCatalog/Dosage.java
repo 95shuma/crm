9ce0e18237bd7e7831789557f8d931f7e2c8b3d3
Baktygul <bakulyakozhoshova@gmail.com>
2020-06-25 22:46:32 +0600

    #75 добавила справочник-сущность Dosage

---
+++ src/main/java/com/project/crm/backend/model/catalog/remediesCatalog/Dosage.java
@@ -0,0 +1,21 @@
+package com.project.crm.backend.model.catalog.remediesCatalog;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Entity
+@Table(name="dosages")
+public class Dosage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Обязательное поле")
+    private String name;
+}
