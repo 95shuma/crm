7716bfe68f1c1e87fa5740847d44d7e85e6dc622
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-06-27 20:19:24 +0600

    #74 Создал DTO для Истории болезней

---
+++ src/main/java/com/project/crm/backend/dto/MedicalHistoryDTO.java
@@ -0,0 +1,48 @@
+package com.project.crm.backend.dto;
+
+
+import com.project.crm.backend.dto.medicalHistoryCatalog.*;
+import com.project.crm.backend.model.catalog.MedicalHistory;
+import com.project.crm.backend.model.catalog.Place;
+import com.project.crm.backend.model.catalog.RecordJournal;
+import com.project.crm.backend.model.catalog.medicalHistoryCatalog.*;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@ToString
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MedicalHistoryDTO {
+
+    private Long id;
+    private RecordJournalDTO recordJournalDTO;
+    private Date date;
+    private boolean typeOfVisit;
+    private String complaint;
+    private DirectionDTO directionDTO;
+    private ExaminationResultDTO examinationResultDTO;
+    private DiagnoseResultDTO diagnoseResultDTO;
+    private String recommendation;
+    private TreatmentDTO treatmentDTO;
+    private SickListDTO sickListDTO;
+
+    public static MedicalHistoryDTO from(MedicalHistory medicalHistory) {
+        return builder()
+                .id(medicalHistory.getId())
+                .recordJournalDTO(RecordJournalDTO.from(medicalHistory.getRecordJournal()))
+                .date(medicalHistory.getDate())
+                .typeOfVisit(medicalHistory.isTypeOfVisit())
+                .complaint(medicalHistory.getComplaint())
+                .directionDTO(DirectionDTO.from(medicalHistory.getDirection()))
+                .examinationResultDTO(ExaminationResultDTO.from(medicalHistory.getExaminationResult()))
+                .diagnoseResultDTO(DiagnoseResultDTO.from(medicalHistory.getDiagnoseResult()))
+                .recommendation(medicalHistory.getRecommendation())
+                .treatmentDTO(TreatmentDTO.from(medicalHistory.getTreatment()))
+                .sickListDTO(SickListDTO.from(medicalHistory.getSickList()))
+                .build();
+    }
+}
