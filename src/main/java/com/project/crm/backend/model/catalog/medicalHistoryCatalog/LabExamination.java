6ef116cbe1bab16e195ac7af955337a8e5da3911
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-06-27 19:40:09 +0600

    #74 Создал сущность Лабораторные обследования

---
+++ src/main/java/com/project/crm/backend/model/catalog/medicalHistoryCatalog/LabExamination.java
@@ -0,0 +1,23 @@
+package com.project.crm.backend.model.catalog.medicalHistoryCatalog;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Entity
+@Table(name="lab_examinations")
+public class LabExamination {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Обязательное поле")
+    private String name;
+
+    private String rate;
+}
