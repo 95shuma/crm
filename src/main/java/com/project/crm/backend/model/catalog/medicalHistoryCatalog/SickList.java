f03be63874eefdfa89fe1d8d3e8c3367e654753c
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-06-27 19:41:13 +0600

    #74 Создал сущность Больничный Лист

---
+++ src/main/java/com/project/crm/backend/model/catalog/medicalHistoryCatalog/SickList.java
@@ -0,0 +1,31 @@
+package com.project.crm.backend.model.catalog.medicalHistoryCatalog;
+
+import com.project.crm.backend.model.catalog.MedicalHistory;
+import com.project.crm.backend.model.catalog.Position;
+import com.project.crm.backend.model.catalog.Remedy;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@Entity
+@Table(name="sick_lists")
+public class SickList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long number;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    @ManyToOne @JoinColumn(name = "medical_history_id")
+    private MedicalHistory medicalHistory;
+}
