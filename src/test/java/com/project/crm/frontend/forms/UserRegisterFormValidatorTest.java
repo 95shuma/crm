bb637e117646008be738c1c9a46f812d633c6dd9
kempl <kempl95.kg@gmail.com>
2020-06-28 19:49:48 +0600

    #73 Создал тест метод, проверяющий длинну ИНН. Ожидает ошибку "Требуется ввести 14 цифр" из переменной ИНН, когда переданный ИНН меньше 14 символов

--- src/test/java/com/project/crm/frontend/forms/UserRegisterFormValidatorTest.java
+++ src/test/java/com/project/crm/frontend/forms/UserRegisterFormValidatorTest.java
@@ -1,6 +1,5 @@
 package com.project.crm.frontend.forms;
 
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
@@ -19,6 +18,7 @@ public class UserRegisterFormValidatorTest {
     private UserRegisterForm userRegisterForm;
 
     private String innMore14;
+    private String innLess14;
     private String correctInn;
     private String correctPassword;
     private String correctDocumentNumber;
@@ -39,6 +39,7 @@ public class UserRegisterFormValidatorTest {
     @Before
     public void setUpParameters(){
         innMore14 = "111111111111111111111111";
+        innLess14 = "1111";
         correctInn = "12345678912345";
         correctPassword = "12345678912345";
         correctDocumentNumber = "ID1234567";
@@ -47,14 +48,13 @@ public class UserRegisterFormValidatorTest {
         correctMiddleName = "Тестович";
         correctGender = "мужской";
     }
-
     @AfterEach
     public void tearDown(){
         userRegisterForm = null;
     }
 
     @Test
-    public void validation_wrongSizeInn_FailValidation() {
+    public void validation_wrongSizeInnMore14_ExpectFailValidation() {
         Calendar calendar = Calendar.getInstance();
         java.util.Date today = calendar.getTime();
 
@@ -76,6 +76,30 @@ public class UserRegisterFormValidatorTest {
         violations.forEach(violation -> assertEquals("inn", violation.getPropertyPath().toString()));
         assertEquals(1, violations.size());
         assertFalse(violations.isEmpty());
+    }
 
+    @Test
+    public void validation_wrongSizeInnLess14_ExpectFailValidation() {
+        Calendar calendar = Calendar.getInstance();
+        java.util.Date today = calendar.getTime();
+
+        userRegisterForm.setInn(innLess14);
+        userRegisterForm.setPassword(correctPassword);
+        userRegisterForm.setDocumentNumber(correctDocumentNumber);
+        userRegisterForm.setName(correctName);
+        userRegisterForm.setSurname(correctSurname);
+        userRegisterForm.setMiddleName(correctMiddleName);
+        userRegisterForm.setBirthDate(today);
+        userRegisterForm.setGender(correctGender);
+        userRegisterForm.setPlaceId((long) 1);
+        userRegisterForm.setHospitalId((long) 1);
+        userRegisterForm.setRoleId((long) 1);
+        userRegisterForm.setPositionId((long) 1);
+
+        Set<ConstraintViolation<UserRegisterForm>> violations = validator.validate(userRegisterForm);
+        violations.forEach(violation -> assertEquals("Требуется ввести 14 цифр", violation.getMessage()));
+        violations.forEach(violation -> assertEquals("inn", violation.getPropertyPath().toString()));
+        assertEquals(1, violations.size());
+        assertFalse(violations.isEmpty());
     }
 }
