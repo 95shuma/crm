b6c16596dd5e144623805a0a376d4a75a762160b
kempl <kempl95.kg@gmail.com>
2020-06-28 19:47:03 +0600

    #73 Создал тест метод, проверяющий длинну ИНН. Ожидает ошибку "Требуется ввести 14 цифр" из переменной ИНН, когда переданный ИНН больше 14 символов

--- src/test/java/com/project/crm/frontend/forms/UserRegisterFormValidatorTest.java
+++ src/test/java/com/project/crm/frontend/forms/UserRegisterFormValidatorTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import javax.validation.*;
+import java.util.*;
+import static org.junit.Assert.*;
 
 @SpringBootTest
 public class UserRegisterFormValidatorTest {
@@ -15,6 +17,15 @@ public class UserRegisterFormValidatorTest {
     private Validator validator;
 
     private UserRegisterForm userRegisterForm;
+
+    private String innMore14;
+    private String correctInn;
+    private String correctPassword;
+    private String correctDocumentNumber;
+    private String correctName;
+    private String correctSurname;
+    private String correctMiddleName;
+    private String correctGender;
 
     @Before
     public void setUp() {
@@ -25,6 +36,17 @@ public class UserRegisterFormValidatorTest {
     public void setUpUserRegisterForm(){
         userRegisterForm = new UserRegisterForm();
     }
+    @Before
+    public void setUpParameters(){
+        innMore14 = "111111111111111111111111";
+        correctInn = "12345678912345";
+        correctPassword = "12345678912345";
+        correctDocumentNumber = "ID1234567";
+        correctName = "Тест";
+        correctSurname = "Тестов";
+        correctMiddleName = "Тестович";
+        correctGender = "мужской";
+    }
 
     @AfterEach
     public void tearDown(){
@@ -33,7 +55,27 @@ public class UserRegisterFormValidatorTest {
 
     @Test
     public void validation_wrongSizeInn_FailValidation() {
+        Calendar calendar = Calendar.getInstance();
+        java.util.Date today = calendar.getTime();
 
+        userRegisterForm.setInn(innMore14);
+        userRegisterForm.setPassword(correctPassword);
+        userRegisterForm.setDocumentNumber(correctDocumentNumber);
+        userRegisterForm.setName(correctName);
+        userRegisterForm.setSurname(correctSurname);
+        userRegisterForm.setMiddleName(correctMiddleName);
+        userRegisterForm.setBirthDate(today);
+        userRegisterForm.setGender(correctGender);
+        userRegisterForm.setPlaceId((long) 1);
+        userRegisterForm.setHospitalId((long) 1);
+        userRegisterForm.setRoleId((long) 1);
+        userRegisterForm.setPositionId((long) 1);
+
+        Set<ConstraintViolation<UserRegisterForm>> violations = validator.validate(userRegisterForm);
+        violations.forEach(violation -> assertEquals("Требуется ввести 14 цифр", violation.getMessage()));
+        violations.forEach(violation -> assertEquals("inn", violation.getPropertyPath().toString()));
+        assertEquals(1, violations.size());
+        assertFalse(violations.isEmpty());
 
     }
 }
