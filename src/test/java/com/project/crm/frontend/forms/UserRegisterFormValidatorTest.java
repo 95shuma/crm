95a3e13b76a762432591eb5bc8e0756f65621790
kempl <kempl95.kg@gmail.com>
2020-06-28 21:30:11 +0600

    #73 Добавил тест метод, проверяющий если placeId = null в будущем, то ожидает сообщение "Обязательное поле" из поля placeId

--- src/test/java/com/project/crm/frontend/forms/UserRegisterFormValidatorTest.java
+++ src/test/java/com/project/crm/frontend/forms/UserRegisterFormValidatorTest.java
@@ -292,4 +292,25 @@ public class UserRegisterFormValidatorTest {
         assertEquals(1, violations.size());
         assertFalse(violations.isEmpty());
     }
+    @Test
+    public void validation_NullPlaceId_ExpectPlaceIdFailValidation() {
+        userRegisterForm.setInn(correctInn);
+        userRegisterForm.setPassword(correctPassword);
+        userRegisterForm.setDocumentNumber(correctDocumentNumber);
+        userRegisterForm.setName(correctName);
+        userRegisterForm.setSurname(correctSurname);
+        userRegisterForm.setMiddleName(correctMiddleName);
+        userRegisterForm.setBirthDate(today);
+        userRegisterForm.setGender(correctGender);
+        userRegisterForm.setPlaceId(null);
+        userRegisterForm.setHospitalId((long) 1);
+        userRegisterForm.setRoleId((long) 1);
+        userRegisterForm.setPositionId((long) 1);
+
+        Set<ConstraintViolation<UserRegisterForm>> violations = validator.validate(userRegisterForm);
+        violations.forEach(violation -> assertEquals("Обязательное поле", violation.getMessage()));
+        violations.forEach(violation -> assertEquals("placeId", violation.getPropertyPath().toString()));
+        assertEquals(1, violations.size());
+        assertFalse(violations.isEmpty());
+    }
 }
