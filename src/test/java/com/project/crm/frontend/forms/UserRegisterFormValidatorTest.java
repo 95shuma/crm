16d218df3d4720a44fb4eb7107f564b41da8302b
kempl <kempl95.kg@gmail.com>
2020-06-28 19:43:01 +0600

    #73 Подключил классы для последующих методов.

--- src/test/java/com/project/crm/frontend/forms/UserRegisterFormValidatorTest.java
+++ src/test/java/com/project/crm/frontend/forms/UserRegisterFormValidatorTest.java
@@ -1,8 +1,39 @@
 package com.project.crm.frontend.forms;
 
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import javax.validation.*;
 
 @SpringBootTest
 public class UserRegisterFormValidatorTest {
-    
+
+    @Autowired
+    private Validator validator;
+
+    private UserRegisterForm userRegisterForm;
+
+    @Before
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+    @Before
+    public void setUpUserRegisterForm(){
+        userRegisterForm = new UserRegisterForm();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        userRegisterForm = null;
+    }
+
+    @Test
+    public void validation_wrongSizeInn_FailValidation() {
+
+
+    }
 }
