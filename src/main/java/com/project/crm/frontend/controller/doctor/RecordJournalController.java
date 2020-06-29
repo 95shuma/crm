5654a1f8f1e9b9fb1b69bb9a2dbf38dd60479253
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-06-29 23:54:14 +0600

    #78 Добавил новый метод в контроллер для страницы Прием пациента

--- src/main/java/com/project/crm/frontend/controller/doctor/RecordJournalController.java
+++ src/main/java/com/project/crm/frontend/controller/doctor/RecordJournalController.java
@@ -1,17 +1,25 @@
 package com.project.crm.frontend.controller.doctor;
 
+import com.project.crm.backend.model.catalog.MedicalHistory;
+import com.project.crm.backend.services.MedicalHistoryService;
 import com.project.crm.backend.services.PropertiesService;
 import com.project.crm.backend.services.RecordJournalService;
 import com.project.crm.backend.services.UserService;
+import com.project.crm.frontend.forms.MedicalHistoryRegisterForm;
+import com.project.crm.frontend.forms.RecordJournalRegisterForm;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.security.Principal;
 
 import static com.project.crm.backend.services.PropertiesService.constructPageable;
@@ -24,6 +32,7 @@ public class RecordJournalController {
     private final RecordJournalService recordJournalService;
     private final UserService userService;
     private final PropertiesService propertiesService;
+    private final MedicalHistoryService medicalHistoryService;
 
 
    @GetMapping
@@ -69,4 +78,36 @@ public class RecordJournalController {
 
         return "/doctor/appointments/appointment";
     }
+
+    @GetMapping("/{record_id}/accept")
+    public String getRecordInfoPageAccept(Model model,Principal principal, @PathVariable("record_id") String record_id) {
+
+        if(principal == null){
+            return "errorPage";
+        }
+
+        userService.checkUserPresence(model, principal);
+        model.addAttribute("journal", recordJournalService.getById(record_id));
+        //model.addAttribute("medicalHistory", medicalHistoryService.getByRecordJournalId(record_id));
+
+        return "/doctor/appointments/appointmentAccept";
+    }
+
+    @PostMapping("/{record_id}/accept")
+    public String createMedicalHistory(@Valid MedicalHistoryRegisterForm medicalHistoryRegisterForm,
+                                       BindingResult validationResult,
+                                       RedirectAttributes attributes, @PathVariable("record_id") String record_id){
+
+        attributes.addFlashAttribute("medicalHistoryRegisterForm", medicalHistoryRegisterForm);
+
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/doctor/records/" + record_id + "/accept";
+        }
+
+        medicalHistoryService.createMedicalHistory(medicalHistoryRegisterForm, record_id);
+
+        return "redirect:/doctor/records";
+    }
+
 }
