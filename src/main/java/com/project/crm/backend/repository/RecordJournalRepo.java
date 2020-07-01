0311b50a5162155d1051d046cd0d48c1bb3e27d9
aasaliev <aasaliev@gmail.com>
2020-07-01 18:27:53 +0600

    #82 Изменил метод findByPatientId, чтобы выводит список с пагинацией

--- src/main/java/com/project/crm/backend/repository/RecordJournalRepo.java
+++ src/main/java/com/project/crm/backend/repository/RecordJournalRepo.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface RecordJournalRepo extends JpaRepository<RecordJournal, Long> {
 
     List<RecordJournal> findByDoctorId(Long id);
-    List<RecordJournal> findByPatientId(Long id);
+    Page<RecordJournal> findByPatientInn(Long inn, Pageable pageable);
 
     Page<RecordJournal> findAllByDoctorIdOrderByDateTime(Long id, Pageable pageable);
 
