c1f3c4ad28e8f66b4b98d86b445063643dece5c7
Baktygul <bakulyakozhoshova@gmail.com>
2020-07-02 01:00:48 +0600

    #90 добавила паттерны Inn, documNumber в UserRegisterForm

--- src/main/java/com/project/crm/frontend/forms/UserRegisterForm.java
+++ src/main/java/com/project/crm/frontend/forms/UserRegisterForm.java
@@ -13,7 +13,7 @@ import java.util.Date;
 public class UserRegisterForm {
 
     @Size(min = 14, max = 14, message = "Требуется ввести 14 цифр")
-    @Pattern(regexp="^\\d+$", message = "ИНН состоит только из цифр : ${validatedValue}")
+    @Pattern(regexp="^([{1}1|2])\\d+$", message = "ИНН состоит только из цифр начинается с 1 или 2 : ${validatedValue}")
     @NotBlank(message = "Это поле не может быть пустым")
     private String inn = "";
 
@@ -22,6 +22,8 @@ public class UserRegisterForm {
     private String password = "";
 
     @NotBlank(message = "Обязательное поле")
+    @Size(min = 7, max = 7, message = "Требуется ввести 7 значений без пробела")
+    @Pattern(regexp="^([{2}ID|AN]).{5}\\d+$", message = "№ докумета начинается с AN или ID и состоит из 5 цифр : ${validatedValue}")
     private String documentNumber = "";
 
     @NotBlank(message = "Обязательное поле")
