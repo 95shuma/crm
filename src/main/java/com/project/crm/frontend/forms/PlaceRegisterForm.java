2ac859d7528182554a63b00318ef3e537fd176f1
Baktygul <bakulyakozhoshova@gmail.com>
2020-07-01 14:22:15 +0600

    #90 добавила, изменила аннотации в PlaceRegisterForm

--- src/main/java/com/project/crm/frontend/forms/PlaceRegisterForm.java
+++ src/main/java/com/project/crm/frontend/forms/PlaceRegisterForm.java
@@ -4,15 +4,20 @@ import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class PlaceRegisterForm {
 
     @NotBlank(message = "Обязательное поле")
+    @Pattern(regexp = "^[^\\d\\s]+$", message = "Название должно содержать только буквы : ${validatedValue}")
     private String name = "";
+
     @NotBlank(message = "Обязательное поле")
+    @Pattern(regexp="^\\d+$", message = "Код места должен содержать только цифры : ${validatedValue}")
     private String codePlace = "";
+
     @NotNull(message = "Обязательное поле")
     private Integer groupCode = null;
 
