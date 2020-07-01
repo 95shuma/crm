fe22ec267ca811f62ac01062dfd6a659cf51e87e
Baktygul <bakulyakozhoshova@gmail.com>
2020-07-01 15:32:23 +0600

    #90 дополнила аннотации в PositionRegisterForm, position.ftlh

--- src/main/java/com/project/crm/frontend/forms/PositionRegisterForm.java
+++ src/main/java/com/project/crm/frontend/forms/PositionRegisterForm.java
@@ -4,12 +4,14 @@ package com.project.crm.frontend.forms;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Setter
 public class PositionRegisterForm {
 
     @NotBlank(message = "Обязательное поле")
+    @Pattern(regexp = "^[^\\d\\s]+$", message = "Название должно содержать только буквы : ${validatedValue}")
     private String name = "";
 
 }
