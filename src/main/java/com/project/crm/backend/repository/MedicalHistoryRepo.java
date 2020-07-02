8fac1140d9986580b450e545eea0306684a849fa
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-07-02 18:04:26 +0600

    #78 Убрал поиск по RecordJournal в MedicalHistoryRepo

--- src/main/java/com/project/crm/backend/repository/MedicalHistoryRepo.java
+++ src/main/java/com/project/crm/backend/repository/MedicalHistoryRepo.java
@@ -4,11 +4,13 @@ import com.project.crm.backend.model.catalog.MedicalHistory;
 import com.project.crm.backend.model.catalog.RecordJournal;
 import com.project.crm.backend.model.catalog.medicalHistoryCatalog.Diagnose;
 import com.project.crm.backend.model.catalog.remediesCatalog.Dosage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MedicalHistoryRepo extends JpaRepository<MedicalHistory, Long> {
     Optional<MedicalHistory> findById(Long id);
-    Optional<MedicalHistory> findByRecordJournal(RecordJournal recordJournal);
 }
