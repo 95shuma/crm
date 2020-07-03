a3a58367faf6bfbe10b777561783f0286fe5b5cd
Baktygul <bakulyakozhoshova@gmail.com>
2020-07-03 23:19:41 +0600

    #92 добавила RemedyRegisterForm

---
+++ src/main/java/com/project/crm/frontend/forms/RemedyRegisterForm.java
@@ -0,0 +1,30 @@
+package com.project.crm.frontend.forms;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import javax.validation.constraints.*;
+
+@Getter
+@Setter
+public class RemedyRegisterForm {
+
+    @NotNull(message = "Обязательное поле")
+    private Long remedyTypeId;
+
+    @NotNull(message = "Обязательное поле")
+    private Long pharmacologicalGroupId;
+
+    @NotNull(message = "Обязательное поле")
+    private Long internationalNameId;
+
+    @NotBlank(message = "Обязательное поле")
+    @Pattern(regexp = "^[^\\d\\s]+$", message = "Название должно содержать только буквы : ${validatedValue}")
+    private String name;
+
+    @NotNull(message = "Обязательное поле")
+    private Long dosageId;
+
+    @NotNull(message = "Обязательное поле")
+    private Long remediesFormId;
+}
