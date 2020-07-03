9c07b67c87e2e51882770e4e1ac3426600c07a5f
Baktygul <bakulyakozhoshova@gmail.com>
2020-07-03 23:39:40 +0600

    #92 добавила List<> getAll в InternationalNameService

--- src/main/java/com/project/crm/backend/services/InternationalNameService.java
+++ src/main/java/com/project/crm/backend/services/InternationalNameService.java
@@ -1,13 +1,20 @@
 package com.project.crm.backend.services;
 
 
+import com.project.crm.backend.dto.PositionDTO;
 import com.project.crm.backend.dto.remediesDto.InternationalNameDTO;
+import com.project.crm.backend.model.catalog.Position;
 import com.project.crm.backend.model.catalog.remediesCatalog.InternationalName;
 import com.project.crm.backend.repository.InternationalNameRepo;
+import com.project.crm.frontend.forms.InternationalNameRegisterForm;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
@@ -20,7 +27,18 @@ public class InternationalNameService {
         return internationalNameRepo.findAll(pageable).map(InternationalNameDTO::from);
     }
 
-    public InternationalNameDTO createInternationalName(InternationalNameDTO internationalNameDTO){
+    public List<InternationalNameDTO> getAll(){
+        List<InternationalName> names = new ArrayList<InternationalName>();
+        names = internationalNameRepo.findAll();
+
+        List<InternationalNameDTO> internationalNameDTO = new ArrayList<InternationalNameDTO>();
+        names.stream().forEach(obj -> {
+            internationalNameDTO.add(InternationalNameDTO.from(obj));
+        });
+        return internationalNameDTO;
+    }
+
+    public InternationalNameDTO createInternationalName(@Valid InternationalNameRegisterForm internationalNameDTO){
         var internationalName = InternationalName.builder()
                 .name(internationalNameDTO.getName())
                 .build();
