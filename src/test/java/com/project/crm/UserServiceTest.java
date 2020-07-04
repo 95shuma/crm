fe6f8b2666256ac3b5cefcabfa05d247db7ecbf3
kempl <kempl95.kg@gmail.com>
2020-07-04 20:57:43 +0600

    #61 Добавил переменные для UserRegisterForm

--- src/test/java/com/project/crm/UserServiceTest.java
+++ src/test/java/com/project/crm/UserServiceTest.java
@@ -8,20 +8,55 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Calendar;
+
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
     @InjectMocks
     private UserService userService;
 
     private UserRegisterForm userRegisterForm;
+    private Calendar calendar;
+    private java.util.Date today;
+    private String correctInn;
+    private String correctPassword;
+    private String correctDocumentNumber;
+    private String correctName;
+    private String correctSurname;
+    private String correctMiddleName;
+    private String correctGender;
+    private String testString;
 
     @Before
     public void setUp(){
         userRegisterForm = new UserRegisterForm();
+        correctInn = "12345678912345";
+        correctPassword = "12345678";
+        correctDocumentNumber = "ID1234567";
+        correctName = "Тест";
+        correctSurname = "Тестов";
+        correctMiddleName = "Тестович";
+        correctGender = "мужской";
+        calendar = Calendar.getInstance();
+        today = calendar.getTime();
+        testString = "тест";
     }
     @Test
     public void createUser_saveCorrectUser_expectSave(){
+        userRegisterForm.setInn(correctInn);
+        userRegisterForm.setPassword(correctPassword);
+        userRegisterForm.setDocumentNumber(correctDocumentNumber);
+        userRegisterForm.setName(correctName);
+        userRegisterForm.setSurname(correctSurname);
+        userRegisterForm.setMiddleName(correctMiddleName);
+        userRegisterForm.setBirthDate(today);
+        userRegisterForm.setGender(correctGender);
+        userRegisterForm.setPlaceId((long) 1);
+        userRegisterForm.setHospitalId((long) 1);
+        userRegisterForm.setRoleId((long) 1);
+        userRegisterForm.setPositionId((long) 1);
 
         userService.createUser(userRegisterForm);
+
     }
 }
