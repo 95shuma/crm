08bd2cc1143d9d694452862d41f9eb47fbcbbedc
kempl <kempl95.kg@gmail.com>
2020-07-04 20:43:56 +0600

    #61 Добавил @Test метод, который будет проверять успешное сохранение метода createUser

--- src/test/java/com/project/crm/UserServiceTest.java
+++ src/test/java/com/project/crm/UserServiceTest.java
@@ -1,9 +1,17 @@
 package com.project.crm;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 
+
+
+    @Test
+    public void createUser_saveCorrectUser_expectSave(){
+
+
+    }
 }
