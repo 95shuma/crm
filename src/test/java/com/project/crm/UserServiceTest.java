5775146a00c7ea5655e6cdeca77df7cd32da7d80
kempl <kempl95.kg@gmail.com>
2020-07-04 20:54:08 +0600

    #61 Добавил проверяемый метод. Инициализировал UserService. Через @InjectMocks инициализируется сервис, используемый при проверке. Добавил инициализацию UserRegisterForm через @Before

--- src/test/java/com/project/crm/UserServiceTest.java
+++ src/test/java/com/project/crm/UserServiceTest.java
@@ -1,17 +1,27 @@
 package com.project.crm;
 
+import com.project.crm.backend.services.UserService;
+import com.project.crm.frontend.forms.UserRegisterForm;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
+    @InjectMocks
+    private UserService userService;
 
+    private UserRegisterForm userRegisterForm;
 
-
+    @Before
+    public void setUp(){
+        userRegisterForm = new UserRegisterForm();
+    }
     @Test
     public void createUser_saveCorrectUser_expectSave(){
 
-
+        userService.createUser(userRegisterForm);
     }
 }
