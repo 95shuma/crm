22e1e1bbfb3f7414e19336d4be6e9084f6b6613b
kempl <kempl95.kg@gmail.com>
2020-07-05 22:30:21 +0600

    #91 Пункт 3: Тест к исправленному методу checkUserPresence. Подтверждает что если в checkUserPresence передать INN, то он найдет согласно роли и сохранит в Model.addAttribute как "user"

--- src/test/java/com/project/crm/backend/services/UserServiceTest.java
+++ src/test/java/com/project/crm/backend/services/UserServiceTest.java
@@ -1,10 +1,12 @@
 package com.project.crm.backend.services;
 
+import com.project.crm.backend.model.User;
 import com.project.crm.backend.model.catalog.Place;
+import com.project.crm.backend.model.catalog.Role;
 import com.project.crm.backend.repository.PlaceRepo;
+import com.project.crm.backend.repository.RegistrationJournalRepo;
+import com.project.crm.backend.repository.RoleRepo;
 import com.project.crm.backend.repository.UserRepo;
-import com.project.crm.backend.services.RegistrationJournalService;
-import com.project.crm.backend.services.UserService;
 import com.project.crm.frontend.forms.UserRegisterForm;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +16,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+
+import java.security.Principal;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 import java.util.Optional;
 import static org.mockito.Mockito.when;
 
@@ -30,7 +38,12 @@ public class UserServiceTest {
     @Mock
     UserRepo userRepo;
     @Mock
+    RoleRepo roleRepo;
+    @Mock
     private RegistrationJournalService registrationJournalService;
+
+    @Mock
+    RegistrationJournalRepo registrationJournalRepo;
 
     private UserRegisterForm userRegisterForm;
     private Calendar calendar;
@@ -44,9 +57,12 @@ public class UserServiceTest {
     private String correctGender;
     private String testString;
 
+    private Model model;
+
     @Before
     public void setUp(){
         userRegisterForm = new UserRegisterForm();
+        model = new ConcurrentModel();
         correctInn = "12345678912345";
         correctPassword = "12345678";
         correctDocumentNumber = "ID1234567";
@@ -82,5 +98,41 @@ public class UserServiceTest {
         userRegisterForm.setPositionId((long) 1);
 
         Assertions.assertDoesNotThrow(() -> userService.createUser((userRegisterForm)));
+    }
+    @Test
+    public void checkUserPresence_getModel_expectModelAttributeSameAsEnteredData(){
+        List<Role> roleList = new ArrayList<>();
+        for (int i = 1; i<=5; i++){
+            roleList.add(Role.builder()
+                    .id(Long.parseLong(Integer.toString(i)))
+                    .name(testString)
+                    .build()
+            );
+        }
+        when(roleRepo.findAll()).thenReturn(roleList);
+        when(registrationJournalRepo.existsByUserInnAndRoleId(Long.parseLong(correctInn), Long.parseLong("5"))).thenReturn(true);       //когда цикл доходит до нужной роли return true. Тем самым понятно что if срабатывает на той роли, на которой нужно
+        when(userRepo.findByInn(Long.parseLong(correctInn))).thenReturn(Optional.of(User.builder()
+                .id((long) 1)
+                .inn(Long.parseLong(correctInn))
+                .password(correctPassword)
+                .documentNumber(correctDocumentNumber)
+                .name(correctName)
+                .surname(correctSurname)
+                .middleName(correctMiddleName)
+                .fullName(correctSurname+" "+correctName+" "+correctMiddleName)
+                .gender(correctGender)
+                .place(Place.builder()
+                        .id((long) 1)
+                        .name(testString)
+                        .codePlace((long) 11111111)
+                        .groupCode((long)1)
+                        .build())
+                .birthDate(today)
+                .build())
+        );
+        Principal principal = () -> correctInn;
+
+        userService.checkUserPresence(model, principal);            //checkUserPresence отрабатывает, model заполняется данными в зависимости от principal.
+        Assertions.assertEquals(model.getAttribute("user") , userRepo.findByInn(Long.parseLong(correctInn)).get());
     }
 }
