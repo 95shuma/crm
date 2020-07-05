b878e090633afa74c89f17a029fb2aa203c21927
kempl <kempl95.kg@gmail.com>
2020-07-06 00:04:12 +0600

    #91 Пункт 1: Переделал тест на createRegistrationJournal на более корректный, так как предыдущий не работал. Текущий тест подтверждает сохранение при указании корректных данных

--- src/test/java/com/project/crm/backend/services/RegistrationJournalServiceTest.java
+++ src/test/java/com/project/crm/backend/services/RegistrationJournalServiceTest.java
@@ -1,61 +1,96 @@
 package com.project.crm.backend.services;
 
 import com.project.crm.backend.model.User;
+import com.project.crm.backend.model.catalog.Place;
+import com.project.crm.backend.model.catalog.Role;
 import com.project.crm.backend.repository.*;
+import com.project.crm.backend.util.Constants;
 import com.project.crm.frontend.forms.UserRegisterForm;
-import org.junit.Assert;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
-
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import static org.mockito.Mockito.when;
+import java.util.Calendar;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class RegistrationJournalServiceTest {
-    @Autowired
+@RunWith(MockitoJUnitRunner.class)
+public class RegistrationJournalServiceTest {
+    @InjectMocks
     private RegistrationJournalService registrationJournalService;
+    @Mock
+    RoleRepo roleRepo;
+    @Mock
+    RegistrationJournalRepo registrationJournalRepo;
 
-    @MockBean
-    private  RegistrationJournalRepo registrationJournalRepo;
+    private UserRegisterForm userRegisterForm;
+    private Calendar calendar;
+    private java.util.Date today;
+    private String correctInn;
+    private String correctPassword;
+    private String correctDocumentNumber;
+    private String correctName;
+    private String correctSurname;
+    private String correctMiddleName;
+    private String correctGender;
+    private String testString;
 
+    @Before
+    public void setUp(){
+        userRegisterForm = new UserRegisterForm();
+        correctInn = "12345678912345";
+        correctPassword = "12345678";
+        correctDocumentNumber = "ID1234567";
+        correctName = "Тест";
+        correctSurname = "Тестов";
+        correctMiddleName = "Тестович";
+        correctGender = "мужской";
+        calendar = Calendar.getInstance();
+        today = calendar.getTime();
+        testString = "тест";
+    }
     @Test
-    void createRegistrationJournal() {
-        User user = new User();
+    public void createRegistrationJournal_saveCorrectUser_expectSave() {
+        when(roleRepo.findByName(Constants.ADMIN)).thenReturn(Optional.of(Role.builder()
+                .id((long) 1)
+                .name(Constants.ADMIN)
+                .build()
+        ));
+        userRegisterForm.setInn(correctInn);
+        userRegisterForm.setPassword(correctPassword);
+        userRegisterForm.setDocumentNumber(correctDocumentNumber);
+        userRegisterForm.setName(correctName);
+        userRegisterForm.setSurname(correctSurname);
+        userRegisterForm.setMiddleName(correctMiddleName);
+        userRegisterForm.setBirthDate(today);
+        userRegisterForm.setGender(correctGender);
+        userRegisterForm.setPlaceId((long) 1);
+        userRegisterForm.setHospitalId((long) 1);
+        userRegisterForm.setRoleId((long) 1);
+        userRegisterForm.setPositionId((long) 1);
 
-        UserRegisterForm admin = new UserRegisterForm();
-        UserRegisterForm doctor = new UserRegisterForm();
-        UserRegisterForm seniorDoctor = new UserRegisterForm();
-        UserRegisterForm juniorDoctor = new UserRegisterForm();
-        UserRegisterForm patient = new UserRegisterForm();
+        User testUser = User.builder()
+                .id((long) 1)
+                .inn(Long.parseLong(correctInn))
+                .password(correctPassword)
+                .documentNumber(correctDocumentNumber)
+                .name(correctName)
+                .surname(correctSurname)
+                .middleName(correctMiddleName)
+                .fullName(correctSurname+" "+correctName+" "+correctMiddleName)
+                .gender(correctGender)
+                .place(Place.builder()
+                        .id((long) 1)
+                        .name(testString)
+                        .codePlace((long) 11111111)
+                        .groupCode((long)1)
+                        .build())
+                .birthDate(today)
+                .build();
 
-        admin.setRoleId((long)1);
-        //------------------------------------------------------
-        doctor.setRoleId((long)2);
-        doctor.setHospitalId((long)2);
-        doctor.setPositionId((long)2);
-        //------------------------------------------------------
-        seniorDoctor.setRoleId((long)3);
-        seniorDoctor.setHospitalId((long)3);
-        seniorDoctor.setPositionId((long)3);
-        //------------------------------------------------------
-        juniorDoctor.setRoleId((long)4);
-        juniorDoctor.setHospitalId((long)4);
-        juniorDoctor.setPositionId((long)4);
-        //------------------------------------------------------
-        patient.setRoleId((long)5);
-        patient.setHospitalId((long)5);
-        //------------------------------------------------------
-
-        registrationJournalService.createRegistrationJournal(user,admin);
-        registrationJournalService.createRegistrationJournal(user,doctor);
-        registrationJournalService.createRegistrationJournal(user,seniorDoctor);
-        registrationJournalService.createRegistrationJournal(user,juniorDoctor);
-        registrationJournalService.createRegistrationJournal(user,patient);
+        Assertions.assertDoesNotThrow(() -> registrationJournalService.createRegistrationJournal(testUser, userRegisterForm));
     }
 }
