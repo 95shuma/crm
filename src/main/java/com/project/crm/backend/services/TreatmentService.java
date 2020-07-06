e5b0f1d8b99aef6eeba62d0fe26961a52e37ab29
95shuma <95shuma@gmail.com>
2020-07-06 08:46:51 +0600

    #86 исправление бага в классе TreatmentService

--- src/main/java/com/project/crm/backend/services/TreatmentService.java
+++ src/main/java/com/project/crm/backend/services/TreatmentService.java
@@ -13,7 +13,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @AllArgsConstructor
@@ -29,7 +28,7 @@ public class TreatmentService {
     }
 
     public Page<TreatmentDTO> getAll(Pageable pageable){
-        return (Page<TreatmentDTO>) treatmentRepo.findAll(pageable).stream().map(TreatmentDTO::from);
+        return treatmentRepo.findAll(pageable).map(TreatmentDTO::from);
     }
 
     public void createTreatment(TreatmentRegisterForm treatmentRegisterForm){
