86d3c9840b8577e60d1bc80ae0eb4fe60c078cea
kempl <kempl95.kg@gmail.com>
2020-07-07 00:34:48 +0600

    #91 Пункт 3: Добавил константы путей и список путей для последующего использования

--- src/main/java/com/project/crm/backend/util/Constants.java
+++ src/main/java/com/project/crm/backend/util/Constants.java
@@ -1,5 +1,8 @@
 package com.project.crm.backend.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Constants {
 
     public static final String ADMIN = "ROLE_ADMIN" ;
@@ -8,8 +11,21 @@ public final class Constants {
     public static final String JUNIOR_DOCTOR = "ROLE_JUNIOR_DOCTOR";
     public static final String PATIENT = "ROLE_PATIENT";
 
+    public static final String REDIRECT_ADMIN = "redirect:/admin";
+    public static final String REDIRECT_SENIOR_DOCTOR = "redirect:/senior-doctor";
+    public static final String REDIRECT_DOCTOR = "redirect:/doctor";
+    public static final String REDIRECT_JUNIOR_DOCTOR = "redirect:/junior-doctor";
+    public static final String REDIRECT_PATIENT = "redirect:/patient";
 
-
+    public static List<String> REDIRECT_LIST(){
+        List<String> list = new ArrayList<>();
+        list.add(REDIRECT_ADMIN);
+        list.add(REDIRECT_SENIOR_DOCTOR);
+        list.add(REDIRECT_DOCTOR);
+        list.add(REDIRECT_JUNIOR_DOCTOR);
+        list.add(REDIRECT_PATIENT);
+        return list;
+    }
 
 
 
