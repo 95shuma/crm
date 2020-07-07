256eded3874a5615c22b4b8cdf4d7bcffd938900
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-07-07 16:37:20 +0600

    #89 добавил метод findAllByMedicalHistoryId

--- src/main/java/com/project/crm/backend/repository/medicalHistoryCatalogRepo/ExaminationResultRepo.java
+++ src/main/java/com/project/crm/backend/repository/medicalHistoryCatalogRepo/ExaminationResultRepo.java
@@ -2,7 +2,11 @@ package com.project.crm.backend.repository.medicalHistoryCatalogRepo;
 
 import com.project.crm.backend.model.catalog.medicalHistoryCatalog.Diagnose;
 import com.project.crm.backend.model.catalog.medicalHistoryCatalog.ExaminationResult;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExaminationResultRepo extends JpaRepository<ExaminationResult, Long> {
+
+    Page<ExaminationResult> findAllByMedicalHistoryId(Long id, Pageable pageable);
 }
