43a297a54de2ec4c2c95a128809b78b2afbbbf90
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-07-08 00:06:17 +0600

    #88 Создал сервис для сохранения направления и получения всех направлений в бд

---
+++ src/main/java/com/project/crm/backend/services/DirectionService.java
@@ -0,0 +1,48 @@
+package com.project.crm.backend.services;
+
+import com.project.crm.backend.dto.medicalHistoryCatalogDTO.DirectionDTO;
+import com.project.crm.backend.dto.medicalHistoryCatalogDTO.TreatmentDTO;
+import com.project.crm.backend.model.catalog.medicalHistoryCatalog.Direction;
+import com.project.crm.backend.model.catalog.medicalHistoryCatalog.Treatment;
+import com.project.crm.backend.repository.MedicalHistoryRepo;
+import com.project.crm.backend.repository.PositionRepo;
+import com.project.crm.backend.repository.RemedyRepo;
+import com.project.crm.backend.repository.medicalHistoryCatalogRepo.*;
+import com.project.crm.frontend.forms.DirectionRegisterForm;
+import com.project.crm.frontend.forms.TreatmentRegisterForm;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class DirectionService {
+
+    private final DirectionRepo directionRepo;
+    private final LabExaminationRepo labExaminationRepo;
+    private final InstrumExaminationRepo instrumExaminationRepo;
+    private final PositionRepo positionRepo;
+    private final MedicalHistoryRepo medicalHistoryRepo;
+
+    public List<DirectionDTO> getAll(){
+        return directionRepo.findAll().stream().map(DirectionDTO::from).collect(Collectors.toList());
+    }
+
+    public Page<DirectionDTO> getAll(Pageable pageable){
+        return directionRepo.findAll(pageable).map(DirectionDTO::from);
+    }
+
+    public void createDirection(DirectionRegisterForm directionRegisterForm){
+        var direction = Direction.builder()
+                .instrumExamination(instrumExaminationRepo.findById(directionRegisterForm.getInstrumExaminationId()).get())
+                .labExamination(labExaminationRepo.findById(directionRegisterForm.getLabExaminationId()).get())
+                .position(positionRepo.findById(directionRegisterForm.getPositionId()).get())
+                .medicalHistory(medicalHistoryRepo.findById(directionRegisterForm.getMedicalHistoryId()).get())
+                .build();
+        directionRepo.save(direction);
+    }
+}
