c57104d441b10979a07d239fe74dfe0585b250d6
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-07-09 16:10:05 +0600

    #87 Добавил новый метод в репо для поиска по medicalHistoryId

--- src/main/java/com/project/crm/backend/repository/medicalHistoryCatalogRepo/DiagnoseResultRepo.java
+++ src/main/java/com/project/crm/backend/repository/medicalHistoryCatalogRepo/DiagnoseResultRepo.java
@@ -2,7 +2,11 @@ package com.project.crm.backend.repository.medicalHistoryCatalogRepo;
 
 import com.project.crm.backend.model.catalog.medicalHistoryCatalog.Diagnose;
 import com.project.crm.backend.model.catalog.medicalHistoryCatalog.DiagnoseResult;
+import com.project.crm.backend.model.catalog.medicalHistoryCatalog.Direction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DiagnoseResultRepo extends JpaRepository<DiagnoseResult, Long> {
+    Page<DiagnoseResult> findAllByMedicalHistoryId(Long id, Pageable pageable);
 }
