df8a8901a59d9b4335fbc351ef0fe1fa012e19f9
95shuma <95shuma@gmail.com>
2020-07-13 10:25:15 +0600

    #97 создал класс для теста TreatmentServiceTest

---
+++ src/test/java/com/project/crm/backend/services/medicalHistoryService/TreatmentServiceTest.java
@@ -0,0 +1,9 @@
+package com.project.crm.backend.services.medicalHistoryService;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TreatmentServiceTest {
+
+}
