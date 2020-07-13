8276a1711c60173f241582f5e1958b27403a97df
95shuma <95shuma@gmail.com>
2020-07-13 10:53:08 +0600

    #97 добавил необходимые поля в классе TreatmentService

--- src/test/java/com/project/crm/backend/services/medicalHistoryService/TreatmentServiceTest.java
+++ src/test/java/com/project/crm/backend/services/medicalHistoryService/TreatmentServiceTest.java
@@ -1,9 +1,47 @@
 package com.project.crm.backend.services.medicalHistoryService;
 
+import com.project.crm.backend.model.catalog.MedicalHistory;
+import com.project.crm.backend.model.catalog.Remedy;
+import com.project.crm.backend.model.catalog.medicalHistoryCatalog.Procedure;
+import com.project.crm.backend.repository.MedicalHistoryRepo;
+import com.project.crm.backend.repository.RemedyRepo;
+import com.project.crm.backend.repository.medicalHistoryCatalogRepo.ProcedureRepo;
+import com.project.crm.backend.repository.medicalHistoryCatalogRepo.TreatmentRepo;
+import com.project.crm.frontend.forms.medicalHistoryForms.TreatmentRegisterForm;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.jupiter.api.Assertions;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TreatmentServiceTest {
+
+    @InjectMocks
+    private TreatmentService treatmentService;
+
+    @Mock
+    TreatmentRepo treatmentRepo;
+
+    @Mock
+    RemedyRepo remedyRepo;
+
+    @Mock
+    ProcedureRepo procedureRepo;
+
+    @Mock
+    MedicalHistoryRepo medicalHistoryRepo;
+
+    private TreatmentRegisterForm treatmentRegisterForm;
+    private String test_remedy_note;
+    private String test_procedure_note;
+    private boolean test_type;
+
+
 
 }
