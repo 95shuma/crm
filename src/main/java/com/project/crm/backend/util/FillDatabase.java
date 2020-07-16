1b64066224c74cda149e53463f402694a1796ce0
kempl <kempl95.kg@gmail.com>
2020-07-16 23:56:19 +0600

    #111 Вынес еще несколько создаваемых данных (Справочники) в отдельные методы.

--- src/main/java/com/project/crm/backend/util/FillDatabase.java
+++ src/main/java/com/project/crm/backend/util/FillDatabase.java
@@ -105,120 +105,18 @@ public class FillDatabase {
             internationalNameRepo.deleteAll();
             pharmacologicalGroupRepo.deleteAll();
 
-            int qty = rn.nextInt(30)+10;
+            int qty = rn.nextInt(20)+10;
             //--------------------------------------------------- Справочники ---------------------------------------------------
-            //--<======================== RemedyType ========================
-            List <RemedyType> typeList = new ArrayList<>();
-            for (int i = 0; i < qty; i++){
-                typeList.add(RemedyType.builder()
-                        .name(faker.name().title())
-                        .build()
-                );
-            }
-            remedyTypeRepo.saveAll(typeList);
-            //-->======================== RemedyType ========================
-            //--<======================== RemediesForm ========================
-            List <RemediesForm> formList = new ArrayList<>();
-            for (int i = 0; i < 6; i++){
-                formList.add(RemediesForm.builder()
-                        .name(faker.name().nameWithMiddle())
-                        .build()
-                );
-            }
-            remediesFormRepo.saveAll(formList);
-            //-->======================== RemediesForm ========================
-
-            //--<======================== PharmacologicalGroup ========================
-            List <PharmacologicalGroup> groupList = new ArrayList<>();
-            for (int i = 0; i < 20; i++){
-                groupList.add(PharmacologicalGroup.builder()
-                        .name(faker.pokemon().name())
-                        .build()
-                );
-            }
-            pharmacologicalGroupRepo.saveAll(groupList);
-            //-->======================== PharmacologicalGroup ========================
-
-            //--<======================== Measure ========================
-            List <Measure> measureList = new ArrayList<>();
-            for (int i = 0; i < 10; i++){
-                measureList.add(Measure.builder()
-                        .name(faker.name().prefix())
-                        .build()
-                );
-            }
-            measureRepo.saveAll(measureList);
-            //-->======================== Measure ========================
-
-            //--<======================== InternationalName ========================
-            List <InternationalName> intNameList = new ArrayList<>();
-            for (int i = 0; i < 20; i++){
-                intNameList.add(InternationalName.builder()
-                        .name(faker.ancient().god())
-                        .build()
-                );
-            }
-            internationalNameRepo.saveAll(intNameList);
-            //-->======================== InternationalName ========================
-
-            //--<======================== Dosage ========================
-            List <Dosage> dosages = new ArrayList<>();
-            for (int i = 0; i < 5; i++){
-                dosages.add(Dosage.builder()
-                        .name(faker.company().name())
-                        .measure(measureRepo.findAll().get(rn.nextInt(measureRepo.findAll().size())))
-                        .quantity(faker.number().numberBetween(10, 30))
-                        .build()
-                );
-            }
-            dosageRepo.saveAll(dosages);
-            //-->======================== Dosage ========================
-
-            //--<======================== Place ========================
-            List <Place> registrationPlaceList = new ArrayList<>();
-            for (int i = 0; i < qty; i++){
-                registrationPlaceList.add(Place.builder()
-                        .name(faker.address().fullAddress())
-                        .codePlace(Long.valueOf(faker.number().numberBetween(10, 30)))
-                        .groupCode(Long.valueOf(0))
-                        .build()
-                );
-            }
-            placeRepo.saveAll(registrationPlaceList);
-            //-->======================== Place ========================
-            //--<======================== Roles ========================
-            List <Role> roleList = new ArrayList<>();
-            String[] roles = {Constants.ROLE_ADMIN, Constants.ROLE_SENIOR_DOCTOR, Constants.ROLE_DOCTOR, Constants.ROLE_JUNIOR_DOCTOR, Constants.ROLE_PATIENT};
-            for (int i=0; i<roles.length; i++){
-                roleRepo.insertRoleWithId(Long.parseLong(Integer.toString(i+1)), roles[i]);
-            }
-            //-->======================== Roles ========================
-            //--<======================== Hospitals ========================
-            List <Hospital> hospitalList = new ArrayList<>();
-            for (int i = 0; i < 5; i++){
-                hospitalList.add(Hospital.builder()
-                        .name(faker.company().name())
-                        .place(placeRepo.findAll().get(rn.nextInt(placeRepo.findAll().size())))
-                        .address(faker.address().streetAddress())
-                        .build()
-                );
-            }
-            hospitalRepo.saveAll(hospitalList);
-            //-->======================== Hospitals ========================
-            //--<======================== Positions ========================
-            List <Position> positionList = new ArrayList<>();
-            String[] positions = {"терапевт", "кардиолог", "лор", "детский врач", "офтальмолог"};
-            for (int i=0; i<positions.length; i++){
-                positionRepo.insertPositionWithId(Long.parseLong(Integer.toString(i+1)), positions[i]);
-            }
-            /*for (int i=0; i<qty; i++){
-                positionList.add(Position.builder()
-                        .name(faker.job().position())
-                        .build()
-                );
-            }
-            positionRepo.saveAll(positionList);*/
-            //-->======================== Positions ========================
+            saveRemedyTypes(remedyTypeRepo, qty);
+            saveRemedyForms(remediesFormRepo, qty);
+            savePharmacologicalGroups(pharmacologicalGroupRepo, qty);
+            saveMeasures(measureRepo, qty);
+            saveInternationalNames(internationalNameRepo, qty);
+            saveDosages(dosageRepo, measureRepo, qty);
+            savePlaces(placeRepo, qty);
+            saveRoles(roleRepo);
+            saveHospitals(hospitalRepo, placeRepo, qty);
+            savePositionsConstant(positionRepo);
             //--------------------------------------------------- Справочники ---------------------------------------------------
             //--<======================== Admin ========================
             String[] adminName = {" ", faker.name().lastName(), faker.name().firstName(), faker.name().lastName()};
@@ -627,6 +525,74 @@ public class FillDatabase {
 
 
     // --< ========================================= SAVE методы =========================================
+
+    public static void saveRemedyTypes(RemedyTypeRepo remedyTypeRepo, int qty) {
+        List <RemedyType> typeList = new ArrayList<>();
+        for (int i = 0; i < qty; i++){
+            typeList.add(RemedyType.builder()
+                    .name(faker.name().title())
+                    .build()
+            );
+        }
+        remedyTypeRepo.saveAll(typeList);
+    }
+
+    public static void saveRemedyForms(RemediesFormRepo remediesFormRepo, int qty) {
+        List <RemediesForm> formList = new ArrayList<>();
+        for (int i = 0; i < qty; i++){
+            formList.add(RemediesForm.builder()
+                    .name(faker.name().nameWithMiddle())
+                    .build()
+            );
+        }
+        remediesFormRepo.saveAll(formList);
+    }
+    public static void savePharmacologicalGroups(PharmacologicalGroupRepo pharmacologicalGroupRepo, int qty) {
+        List <PharmacologicalGroup> groupList = new ArrayList<>();
+        for (int i = 0; i < qty; i++){
+            groupList.add(PharmacologicalGroup.builder()
+                    .name(faker.pokemon().name())
+                    .build()
+            );
+        }
+        pharmacologicalGroupRepo.saveAll(groupList);
+    }
+    public static void saveMeasures(MeasureRepo measureRepo, int qty) {
+        List <Measure> measureList = new ArrayList<>();
+        for (int i = 0; i < qty; i++){
+            measureList.add(Measure.builder()
+                    .name(faker.name().prefix())
+                    .build()
+            );
+        }
+        measureRepo.saveAll(measureList);
+    }
+    public static void saveInternationalNames(InternationalNameRepo internationalNameRepo, int qty) {
+        List <InternationalName> intNameList = new ArrayList<>();
+        for (int i = 0; i < qty; i++){
+            intNameList.add(InternationalName.builder()
+                    .name(faker.ancient().god())
+                    .build()
+            );
+        }
+        internationalNameRepo.saveAll(intNameList);
+    }
+    public static void saveDosages(DosageRepo dosageRepo, MeasureRepo measureRepo, int qty) {
+        List <Dosage> dosages = new ArrayList<>();
+        for (int i = 0; i < qty; i++){
+            dosages.add(Dosage.builder()
+                    .name(faker.company().name())
+                    .measure(measureRepo.findAll().get(rn.nextInt(measureRepo.findAll().size())))
+                    .quantity(faker.number().numberBetween(10, 30))
+                    .build()
+            );
+        }
+        dosageRepo.saveAll(dosages);
+    }
+    public static void saveDosagesWithMeasureRepo(DosageRepo dosageRepo, MeasureRepo measureRepo, int qty) {
+        saveMeasures(measureRepo, qty);
+        saveDosages(dosageRepo, measureRepo, qty);
+    }
     public static void savePlaces(PlaceRepo placeRepo, int size) {
         List<Place> placeList = new ArrayList<>();
         for (int i = 0; i < size; i++) {
@@ -639,25 +605,15 @@ public class FillDatabase {
         }
         placeRepo.saveAll(placeList);
     }
-
     public static void saveRoles(RoleRepo roleRepo){
         String[] roles = {Constants.ROLE_ADMIN, Constants.ROLE_SENIOR_DOCTOR, Constants.ROLE_DOCTOR, Constants.ROLE_JUNIOR_DOCTOR, Constants.ROLE_PATIENT};
         for (int i=0; i<roles.length; i++){
             roleRepo.insertRoleWithId(Long.parseLong(Integer.toString(i+1)), roles[i]);
         }
     }
-
-    public static void savePositions(PositionRepo positionRepo){
-        String[] positions = {"терапевт", "кардиолог", "лор", "детский врач", "офтальмолог"};
-        for (int i=0; i<positions.length; i++){
-            positionRepo.insertPositionWithId(Long.parseLong(Integer.toString(i+1)), positions[i]);
-        }
-    }
-    //Для создания Hospitals в БД должны быть Places поэтому сохраняем вместе
-    public static void saveHospitalsWithPlaces(HospitalRepo hospitalRepo, PlaceRepo placeRepo,  int size){
-        savePlaces(placeRepo, size);
+    public static void saveHospitals(HospitalRepo hospitalRepo, PlaceRepo placeRepo,  int qty){
         List<Hospital> hospitalList = new ArrayList<>();
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < qty; i++){
             hospitalList.add(Hospital.builder()
                     .name(faker.company().name())
                     .place(placeRepo.findAll().get(rn.nextInt(placeRepo.findAll().size())))
@@ -667,10 +623,26 @@ public class FillDatabase {
         }
         hospitalRepo.saveAll(hospitalList);
     }
-
-
-
-
+    public static void saveHospitalsWithPlaces(HospitalRepo hospitalRepo, PlaceRepo placeRepo,  int qty){
+        savePlaces(placeRepo, qty);
+        saveHospitals(hospitalRepo, placeRepo, qty);
+    }
+    public static void savePositionsConstant(PositionRepo positionRepo){
+        String[] positions = {"терапевт", "кардиолог", "лор", "детский врач", "офтальмолог"};
+        for (int i=0; i<positions.length; i++){
+            positionRepo.insertPositionWithId(Long.parseLong(Integer.toString(i+1)), positions[i]);
+        }
+    }
+    public static void savePositionsRandom(PositionRepo positionRepo, int qty){
+        List<Position> positionList = new ArrayList<>();
+        for (int i=0; i<qty; i++){
+            positionList.add(Position.builder()
+                    .name(faker.job().position())
+                    .build()
+            );
+        }
+        positionRepo.saveAll(positionList);
+    }
     // --> ========================================= SAVE методы =========================================
     // --< ========================================= DELETE методы =========================================
     public static void deleteAllData(UserRepo userRepo, PlaceRepo placeRepo, RoleRepo roleRepo,
