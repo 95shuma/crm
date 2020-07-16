d7074adfd7c87cfaf40993524a61646e45bf838d
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-07-16 16:18:56 +0600

    #104 Написал тест для проверки ошибок в поле Жалоба (забыл удалить не нужное)

--- src/test/java/com/project/crm/ui/steps/PatientRecordJournal.java
+++ src/test/java/com/project/crm/ui/steps/PatientRecordJournal.java
@@ -12,33 +12,6 @@ public class PatientRecordJournal extends Steps{
         webDriver.get("http://localhost:7777/login");
     }
 
-    @Ктомуже("изначально врач создает график работы")
-    public void изначальноВрачСоздаетГрафикРаботы(){
-        WebElement username = webDriver.findElement(By.name("username"));
-        username.sendKeys("33333333333333");
-        WebElement password = webDriver.findElement(By.name("password"));
-        password.sendKeys("33333333333333");
-        password.submit();
-        webDriver.get("http://localhost:7777/doctor/schedules/schedule");
-        WebElement day = webDriver.findElement(By.id("dateId"));
-        WebElement dayStartId = webDriver.findElement(By.id("dayStartId"));
-        WebElement dayEndId = webDriver.findElement(By.id("dayEndId"));
-        WebElement lunchStartId = webDriver.findElement(By.id("lunchStartId"));
-        WebElement lunchEndId = webDriver.findElement(By.id("lunchEndId"));
-        WebElement button = webDriver.findElement(By.id("button"));
-        day.clear();
-        day.sendKeys("15.07.2020");
-        dayStartId.clear();
-        dayStartId.sendKeys("15.07.2020 22:00");
-        dayEndId.clear();
-        dayEndId.sendKeys("15.07.2020 23:59");
-        lunchStartId.clear();
-        lunchStartId.sendKeys("16.07.2020 00:00");
-        lunchEndId.clear();
-        lunchEndId.sendKeys("16.07.2020 00:00");
-        button.submit();
-    }
-
     @Когда("пациент попадает к себе на страницу")
     public void пациентПопадаетКСебеНаСтраницу(){
         WebElement username = webDriver.findElement(By.name("username"));
