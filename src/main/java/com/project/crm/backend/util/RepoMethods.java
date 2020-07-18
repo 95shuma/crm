ae5a412c8a033195d024aff405fbebf8fc025ed2
kempl <kempl95.kg@gmail.com>
2020-07-18 13:01:44 +0600

    #111 Добавил описание структуры правильного названия методов. Переделал наименования под общую структуру и вынес переменные qty в начало

--- src/main/java/com/project/crm/backend/util/RepoMethods.java
+++ src/main/java/com/project/crm/backend/util/RepoMethods.java
@@ -22,8 +22,15 @@ public class RepoMethods {
 
 
     // --< ========================================= SAVE методы =========================================
+    // Струтура наименовий методов: save + Repo + Type/With
+    //  Type:
+    //      Constant -> сохранят те данные, которые передаются в параметры
+    //      Random -> Генерирует полностью рандомный список
+    //      Если не указано -> Random по умолчанию
+    //  With: -> Сохраняются несколько репозиториев.
+    //          Например: saveHospitalsWithPlaces() -> Так как для сохранения  Hospitals нужны Places, сначала сделает savePlaces, потом saveHospitals. Чтобы не вызывать 2 метода подряд.
 
-    public static void saveRemedyTypes(RemedyTypeRepo remedyTypeRepo, int qty) {
+    public static void saveRemedyTypes(int qty, RemedyTypeRepo remedyTypeRepo) {
         List<RemedyType> typeList = new ArrayList<>();
         for (int i = 0; i < qty; i++){
             typeList.add(RemedyType.builder()
@@ -34,7 +41,7 @@ public class RepoMethods {
         remedyTypeRepo.saveAll(typeList);
     }
 
-    public static void saveRemedyForms(RemediesFormRepo remediesFormRepo, int qty) {
+    public static void saveRemedyForms(int qty, RemediesFormRepo remediesFormRepo) {
         List <RemediesForm> formList = new ArrayList<>();
         for (int i = 0; i < qty; i++){
             formList.add(RemediesForm.builder()
@@ -44,7 +51,7 @@ public class RepoMethods {
         }
         remediesFormRepo.saveAll(formList);
     }
-    public static void savePharmacologicalGroups(PharmacologicalGroupRepo pharmacologicalGroupRepo, int qty) {
+    public static void savePharmacologicalGroups(int qty, PharmacologicalGroupRepo pharmacologicalGroupRepo) {
         List <PharmacologicalGroup> groupList = new ArrayList<>();
         for (int i = 0; i < qty; i++){
             groupList.add(PharmacologicalGroup.builder()
@@ -54,7 +61,7 @@ public class RepoMethods {
         }
         pharmacologicalGroupRepo.saveAll(groupList);
     }
-    public static void saveMeasures(MeasureRepo measureRepo, int qty) {
+    public static void saveMeasures(int qty, MeasureRepo measureRepo) {
         List <Measure> measureList = new ArrayList<>();
         for (int i = 0; i < qty; i++){
             measureList.add(Measure.builder()
@@ -64,7 +71,7 @@ public class RepoMethods {
         }
         measureRepo.saveAll(measureList);
     }
-    public static void saveInternationalNames(InternationalNameRepo internationalNameRepo, int qty) {
+    public static void saveInternationalNames(int qty, InternationalNameRepo internationalNameRepo) {
         List <InternationalName> intNameList = new ArrayList<>();
         for (int i = 0; i < qty; i++){
             intNameList.add(InternationalName.builder()
@@ -74,7 +81,7 @@ public class RepoMethods {
         }
         internationalNameRepo.saveAll(intNameList);
     }
-    public static void saveDosages(DosageRepo dosageRepo, MeasureRepo measureRepo, int qty) {
+    public static void saveDosages(int qty, DosageRepo dosageRepo, MeasureRepo measureRepo) {
         List <Dosage> dosages = new ArrayList<>();
         for (int i = 0; i < qty; i++){
             dosages.add(Dosage.builder()
@@ -86,13 +93,13 @@ public class RepoMethods {
         }
         dosageRepo.saveAll(dosages);
     }
-    public static void saveDosagesWithMeasureRepo(DosageRepo dosageRepo, MeasureRepo measureRepo, int qty) {
-        saveMeasures(measureRepo, qty);
-        saveDosages(dosageRepo, measureRepo, qty);
+    public static void saveDosagesWithMeasureRepo(int qty, DosageRepo dosageRepo, MeasureRepo measureRepo) {
+        saveMeasures(qty, measureRepo);
+        saveDosages(qty, dosageRepo, measureRepo);
     }
-    public static void savePlaces(PlaceRepo placeRepo, int size) {
+    public static void savePlaces(int qty, PlaceRepo placeRepo) {
         List<Place> placeList = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < qty; i++) {
             placeList.add(Place.builder()
                     .name(faker.address().fullAddress())
                     .codePlace((long) faker.number().numberBetween(10, 30))
@@ -102,13 +109,13 @@ public class RepoMethods {
         }
         placeRepo.saveAll(placeList);
     }
-    public static void saveRoles(RoleRepo roleRepo){
+    public static void saveRolesConstant(RoleRepo roleRepo){
         String[] roles = {Constants.ROLE_ADMIN, Constants.ROLE_SENIOR_DOCTOR, Constants.ROLE_DOCTOR, Constants.ROLE_JUNIOR_DOCTOR, Constants.ROLE_PATIENT};
         for (int i=0; i<roles.length; i++){
             roleRepo.insertRoleWithId(Long.parseLong(Integer.toString(i+1)), roles[i]);
         }
     }
-    public static void saveHospitals(HospitalRepo hospitalRepo, PlaceRepo placeRepo,  int qty){
+    public static void saveHospitals(int qty, HospitalRepo hospitalRepo, PlaceRepo placeRepo){
         List<Hospital> hospitalList = new ArrayList<>();
         for (int i = 0; i < qty; i++){
             hospitalList.add(Hospital.builder()
@@ -120,9 +127,9 @@ public class RepoMethods {
         }
         hospitalRepo.saveAll(hospitalList);
     }
-    public static void saveHospitalsWithPlaces(HospitalRepo hospitalRepo, PlaceRepo placeRepo,  int qty){
-        savePlaces(placeRepo, qty);
-        saveHospitals(hospitalRepo, placeRepo, qty);
+    public static void saveHospitalsWithPlaces(int qty, HospitalRepo hospitalRepo, PlaceRepo placeRepo){
+        savePlaces(qty, placeRepo);
+        saveHospitals(qty, hospitalRepo, placeRepo);
     }
     public static void savePositionsConstant(PositionRepo positionRepo){
         String[] positions = {"терапевт", "кардиолог", "лор", "детский врач", "офтальмолог"};
@@ -130,7 +137,7 @@ public class RepoMethods {
             positionRepo.insertPositionWithId(Long.parseLong(Integer.toString(i+1)), positions[i]);
         }
     }
-    public static void savePositionsRandom(PositionRepo positionRepo, int qty){
+    public static void savePositionsRandom(int qty, PositionRepo positionRepo){
         List<Position> positionList = new ArrayList<>();
         for (int i=0; i<qty; i++){
             positionList.add(Position.builder()
@@ -140,7 +147,7 @@ public class RepoMethods {
         }
         positionRepo.saveAll(positionList);
     }
-    public static void saveAdminByInnAndPassword(String inn, String password, UserRepo userRepo, RoleRepo roleRepo, RegistrationJournalRepo registrationJournalRepo){
+    public static void saveAdminConstant(String inn, String password, UserRepo userRepo, RoleRepo roleRepo, RegistrationJournalRepo registrationJournalRepo){
         String[] adminName = {" ", faker.name().lastName(), faker.name().firstName(), faker.name().lastName()};
 
         var user = User.builder()
@@ -160,28 +167,30 @@ public class RepoMethods {
         saveUserInRegistrationJournal(userRepo.findByInn(Long.parseLong(inn)).get(), roleRepo.findByName(Constants.ROLE_ADMIN).get(), null, null, registrationJournalRepo);
     }
     public static void saveAdminWithRoles(String inn, String password, UserRepo userRepo, RoleRepo roleRepo, RegistrationJournalRepo registrationJournalRepo){
-        saveRoles(roleRepo);
-        saveAdminByInnAndPassword(inn, password, userRepo, roleRepo, registrationJournalRepo);
+        saveRolesConstant(roleRepo);
+        saveAdminConstant(inn, password, userRepo, roleRepo, registrationJournalRepo);
     }
-    public static void saveUserRandomByRole(String roleName, UserRepo userRepo, RoleRepo roleRepo, HospitalRepo hospitalRepo, PlaceRepo placeRepo, PositionRepo positionRepo, RegistrationJournalRepo registrationJournalRepo){
-        String inn = getUniqueINN(userRepo);
-        User user = User.builder()
-                .inn(Long.parseLong(inn))
-                .password(passwordEncoder.encode(inn))
-                .documentNumber("ID".concat(faker.number().digits(7)))
-                .fullName(faker.name().fullName())
-                .surname(faker.name().lastName())
-                .name(faker.name().firstName())
-                .middleName(faker.name().lastName())
-                .birthDate(faker.date().birthday())
-                .gender(getRandomGender())
-                .place(placeRepo.findAll().get(rn.nextInt(placeRepo.findAll().size())))
-                .enabled(true)
-                .build();
-        userRepo.save(user);
-        saveUserInRegistrationJournal(userRepo.findByInn(Long.parseLong(inn)).get(), roleRepo.findByName(roleName).get(), positionRepo.findAll().get(positionRepo.findAll().size()), hospitalRepo.findAll().get(rn.nextInt(hospitalRepo.findAll().size())), registrationJournalRepo);
+    public static void saveUserRandom(int qty, String roleName, UserRepo userRepo, RoleRepo roleRepo, HospitalRepo hospitalRepo, PlaceRepo placeRepo, PositionRepo positionRepo, RegistrationJournalRepo registrationJournalRepo){
+        for (int i=0;i<qty;i++){
+            String inn = getUniqueINN(userRepo);
+            User user = User.builder()
+                    .inn(Long.parseLong(inn))
+                    .password(passwordEncoder.encode(inn))
+                    .documentNumber("ID".concat(faker.number().digits(7)))
+                    .fullName(faker.name().fullName())
+                    .surname(faker.name().lastName())
+                    .name(faker.name().firstName())
+                    .middleName(faker.name().lastName())
+                    .birthDate(faker.date().birthday())
+                    .gender(getRandomGender())
+                    .place(placeRepo.findAll().get(rn.nextInt(placeRepo.findAll().size())))
+                    .enabled(true)
+                    .build();
+            userRepo.save(user);
+            saveUserInRegistrationJournal(userRepo.findByInn(Long.parseLong(inn)).get(), roleRepo.findByName(roleName).get(), positionRepo.findAll().get(positionRepo.findAll().size()), hospitalRepo.findAll().get(rn.nextInt(hospitalRepo.findAll().size())), registrationJournalRepo);
+        }
     }
-    public static void saveUserConstantByRole(String inn, String password, String roleName, Place place, Hospital hospital, Position position, UserRepo userRepo, RoleRepo roleRepo, RegistrationJournalRepo registrationJournalRepo){
+    public static void saveUserConstant(String inn, String password, String roleName, Place place, Hospital hospital, Position position, UserRepo userRepo, RoleRepo roleRepo, RegistrationJournalRepo registrationJournalRepo){
         User user = User.builder()
                 .inn(Long.parseLong(inn))
                 .password(passwordEncoder.encode(password))
@@ -198,24 +207,24 @@ public class RepoMethods {
         userRepo.save(user);
         saveUserInRegistrationJournal(user, roleRepo.findByName(roleName).get(), position, hospital, registrationJournalRepo);
     }
-    public static void saveDoctorsWithRandomInnForEachHospital(int doctorQty, int juniorDoctorQty,UserRepo userRepo, HospitalRepo hospitalRepo, PositionRepo positionRepo, RoleRepo roleRepo, RegistrationJournalRepo registrationJournalRepo){
+    public static void saveDoctorsRandomForEachHospital(int doctorQty, int juniorDoctorQty,UserRepo userRepo, HospitalRepo hospitalRepo, PositionRepo positionRepo, RoleRepo roleRepo, RegistrationJournalRepo registrationJournalRepo){
         for (Hospital hospital : hospitalRepo.findAll()) {
             String inn = getUniqueINN(userRepo);
-            saveUserConstantByRole(inn, inn, Constants.ROLE_SENIOR_DOCTOR, hospital.getPlace(), hospital, positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
+            saveUserConstant(inn, inn, Constants.ROLE_SENIOR_DOCTOR, hospital.getPlace(), hospital, positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
             for (int i=0; i<doctorQty; i++) {
                 inn = getUniqueINN(userRepo);
-                saveUserConstantByRole(inn, inn, Constants.ROLE_DOCTOR, hospital.getPlace(), hospital, positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
+                saveUserConstant(inn, inn, Constants.ROLE_DOCTOR, hospital.getPlace(), hospital, positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
             }
             for (int i=0; i<juniorDoctorQty; i++) {
                 inn = getUniqueINN(userRepo);
-                saveUserConstantByRole(inn, inn, Constants.ROLE_JUNIOR_DOCTOR, hospital.getPlace(), hospital, positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
+                saveUserConstant(inn, inn, Constants.ROLE_JUNIOR_DOCTOR, hospital.getPlace(), hospital, positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
             }
         }
     }
     public static void saveDoctorsConstant(UserRepo userRepo, HospitalRepo hospitalRepo, RoleRepo roleRepo, PositionRepo positionRepo, RegistrationJournalRepo registrationJournalRepo){
-        saveUserConstantByRole(Constants.SENIOR_DOCTOR_INN, Constants.SENIOR_DOCTOR_PASSWORD, Constants.ROLE_SENIOR_DOCTOR, hospitalRepo.findAll().get(0).getPlace(), hospitalRepo.findAll().get(0), positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
-        saveUserConstantByRole(Constants.DOCTOR_INN, Constants.DOCTOR_PASSWORD, Constants.ROLE_DOCTOR, hospitalRepo.findAll().get(0).getPlace(), hospitalRepo.findAll().get(0), positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
-        saveUserConstantByRole(Constants.JUNIOR_DOCTOR_INN, Constants.JUNIOR_DOCTOR_PASSWORD, Constants.ROLE_JUNIOR_DOCTOR, hospitalRepo.findAll().get(0).getPlace(), hospitalRepo.findAll().get(0), positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
+        saveUserConstant(Constants.SENIOR_DOCTOR_INN, Constants.SENIOR_DOCTOR_PASSWORD, Constants.ROLE_SENIOR_DOCTOR, hospitalRepo.findAll().get(0).getPlace(), hospitalRepo.findAll().get(0), positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
+        saveUserConstant(Constants.DOCTOR_INN, Constants.DOCTOR_PASSWORD, Constants.ROLE_DOCTOR, hospitalRepo.findAll().get(0).getPlace(), hospitalRepo.findAll().get(0), positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
+        saveUserConstant(Constants.JUNIOR_DOCTOR_INN, Constants.JUNIOR_DOCTOR_PASSWORD, Constants.ROLE_JUNIOR_DOCTOR, hospitalRepo.findAll().get(0).getPlace(), hospitalRepo.findAll().get(0), positionRepo.findAll().get(rn.nextInt(positionRepo.findAll().size())), userRepo, roleRepo, registrationJournalRepo);
     }
     // --> ========================================= SAVE методы =========================================
     // --< ========================================= DELETE методы =========================================
