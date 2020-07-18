6d214b61034e100401cb8efa4843185e65db492f
Baktygul <bakulyakozhoshova@gmail.com>
2020-07-18 13:41:11 +0600

    #105 дополнение в тесте регистрации доктора, проверка фамилии и даты

--- src/test/java/com/project/crm/ui/steps/DoctorRegisterBySenior.java
+++ src/test/java/com/project/crm/ui/steps/DoctorRegisterBySenior.java
@@ -6,10 +6,7 @@ import io.cucumber.java.Before;
 import io.cucumber.java.ru.*;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -87,15 +84,18 @@ public class DoctorRegisterBySenior extends Steps {
 
     @Тогда("не добавится, выйдут ошибка1")
     public void неДобавитсяВыйдутОшибка1() {
-        //Assertions.assertEquals("Обязательное поле", webDriver.findElement(By.className("passportN")).getText());
-        //Assertions.assertr("Требуется ввести 9 значений без пробела", webDriver.findElement(By.className("passportN")).getText());
-        // Assertions.assertEquals("№ докумета начинается с AN или ID и состоит из 7 цифр :", webDriver.findElement(By.className("passportN")).getText());
+        List<String> expectedAlert = new ArrayList<String>();
+        List<String> actualAlert = new ArrayList<String>();
 
-        List<String> errorMessages = new ArrayList<String>();
-        String error = webDriver.findElement(By.xpath("passportN")).getText();
-        assertEquals(error,3,3);
-        assertTrue(errorMessages.containsAll(Arrays.asList("Обязательное поле","Требуется ввести 9 значений без пробела","№ докумета начинается с AN или ID и состоит из 7 цифр :")));
-        //assertFalse(violations.isEmpty());
+        actualAlert.add(webDriver.findElement(By.xpath("//div[@class='alert alert-warning mt-1 passportN'][text()='Обязательное поле']")).getText());
+        actualAlert.add(webDriver.findElement(By.xpath("//div[@class='alert alert-warning mt-1 passportN'][text()='№ докумета начинается с AN или ID и состоит из 7 цифр : ']")).getText());
+        actualAlert.add(webDriver.findElement(By.xpath("//div[@class='alert alert-warning mt-1 passportN'][text()='Требуется ввести 9 значений без пробела']")).getText());
+
+        expectedAlert.add("Обязательное поле");
+        expectedAlert.add("№ докумета начинается с AN или ID и состоит из 7 цифр :");
+        expectedAlert.add("Требуется ввести 9 значений без пробела");
+
+        assertEquals(expectedAlert,actualAlert);
     }
     @Когда("админ ЛПУ заполняет неправильно № паспорта")
     public void заполняетНеправильно() {
@@ -115,7 +115,6 @@ public class DoctorRegisterBySenior extends Steps {
 
     @Тогда("не добавится, выйдут ошибка2")
     public void неДобавитсяВыйдутОшибка2() {
-        // Assertions.assertEquals("№ докумета начинается с AN или ID и состоит из 7 цифр : AN123456g", webDriver.findElement(By.className("passportN")).getText());
         Assertions.assertEquals("№ докумета начинается с AN или ID и состоит из 7 цифр : 123456789", webDriver.findElement(By.className("passportN")).getText());
     }
 
@@ -138,5 +137,59 @@ public class DoctorRegisterBySenior extends Steps {
     public void неДобавитсяВыйдутОшибка3() {
         Assertions.assertEquals("Требуется ввести 9 значений без пробела", webDriver.findElement(By.className("passportN")).getText());
     }
+    @Когда("админ ЛПУ  заполняет фамилию неправильно")
+    public void заполняетФамилиюНеправильно() {
+        webDriver.findElement(By.name("inn")).sendKeys("12345678912345");
+        webDriver.findElement(By.name("password")).sendKeys("12345678");
+        webDriver.findElement(By.name("documentNumber")).sendKeys("AN1234567");
+        webDriver.findElement(By.name("surname")).sendKeys("sssss88888");
+        webDriver.findElement(By.name("name")).sendKeys("лшь");
+        webDriver.findElement(By.name("middleName")).sendKeys("ait");
+        webDriver.findElement(By.name("birthDate")).sendKeys("20.01.2010");
+        webDriver.findElement(By.name("gender")).sendKeys("1");
+        webDriver.findElement(By.name("placeId")).sendKeys("2");
+        webDriver.findElement(By.name("positionId")).sendKeys("3");
+        webDriver.findElement(By.name("hospitalId")).sendKeys("4");
+        webDriver.findElement(By.name("name")).submit();
+    }
+    @Тогда("выйдет ошибка Фамилия должна содержать только буквы")
+    public void выйдетОшибкаФамилияДолжнаСодержатьТолькоБуквы(){
+        Assertions.assertEquals("Фамилия должна содержать только буквы : sssss88888",  webDriver.findElement(By.xpath("//div[@class='alert alert-warning mt-1'][text()='Фамилия должна содержать только буквы : sssss88888']")).getText());
+    }
 
+    @Когда("админ ЛПУ  пропустил дату рождения")
+    public void пропустилДатуРождения() {
+        webDriver.findElement(By.name("inn")).sendKeys("12345678912345");
+        webDriver.findElement(By.name("password")).sendKeys("12345678");
+        webDriver.findElement(By.name("documentNumber")).sendKeys("AN1234567");
+        webDriver.findElement(By.name("surname")).sendKeys("sssss");
+        webDriver.findElement(By.name("name")).sendKeys("лшь");
+        webDriver.findElement(By.name("middleName")).sendKeys("ait");
+        webDriver.findElement(By.name("birthDate")).sendKeys("");
+        webDriver.findElement(By.name("gender")).sendKeys("1");
+        webDriver.findElement(By.name("placeId")).sendKeys("2");
+        webDriver.findElement(By.name("positionId")).sendKeys("3");
+        webDriver.findElement(By.name("hospitalId")).sendKeys("4");
+        webDriver.findElement(By.name("name")).submit();
+    }
+
+    @Тогда("выйдет ошибка Обязательное поле для даты")
+    public void ошибкаДаты() {
+
+        Assertions.assertEquals("Обязательное поле",  webDriver.findElement(By.xpath("//div[@class='alert alert-warning mt-1'][text()='Обязательное поле']")).getText());
+
+
+    //List<String> expectedAlert = new ArrayList<String>();
+       // List<String> actualAlert = new ArrayList<String>();
+
+     //   actualAlert.add(webDriver.findElement(By.xpath("//div[@class='alert alert-warning mt-1 passportN'][text()='Обязательное поле']")).getText());
+      //  actualAlert.add(webDriver.findElement(By.xpath("//div[@class='alert alert-warning mt-1 passportN'][text()='№ докумета начинается с AN или ID и состоит из 7 цифр : ']")).getText());
+       // actualAlert.add(webDriver.findElement(By.xpath("//div[@class='alert alert-warning mt-1 passportN'][text()='Дата рождения не может быть в будущем времени']")).getText());
+
+        //expectedAlert.add("Обязательное поле");
+      //  expectedAlert.add("№ докумета начинается с AN или ID и состоит из 7 цифр :");
+       // expectedAlert.add("Дата рождения не может быть в будущем времени");
+
+        //assertEquals(expectedAlert,actualAlert);
+    }
 }
