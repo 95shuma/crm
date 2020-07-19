4099e7dea6319e94968326cf731d7ccc8630b42a
TemirlanSadykov <temirlansadykov2004@gmail.com>
2020-07-19 23:02:11 +0600

    #103 Привел к общему ввиду как в других тестах

--- src/test/java/com/project/crm/ui/steps/DoctorAppointment.java
+++ src/test/java/com/project/crm/ui/steps/DoctorAppointment.java
@@ -1,43 +1,37 @@
 package com.project.crm.ui.steps;
 
-import com.project.crm.frontend.forms.RecordJournalRegisterForm;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.ru.Допустим;
 import io.cucumber.java.ru.И;
-import io.cucumber.java.ru.Когда;
 import io.cucumber.java.ru.Тогда;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.validation.ConstraintViolation;
-import javax.validation.Validator;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 
 public class DoctorAppointment extends Steps{
 
-    @Autowired
-    private Validator validator;
+    @Before("@doctorAppointment")
+    public void start() {
+        setUp();
+    }
+
+    @After("@doctorAppointment")
+    public void finish() {
+        tearDown();
+    }
 
     @Допустим("врач входит на сайт и попадает к себе на страницу")
-    public void врачВходитНаСайт() {
-        webDriver.get("http://localhost:7777/login");
-        WebElement username = webDriver.findElement(By.name("username"));
-        username.sendKeys("33333333333333");
-        WebElement password = webDriver.findElement(By.name("password"));
-        password.sendKeys("33333333333333");
-        password.submit();
+    public void врачВходитНаСайтПопадаетКСебеНаСтраницу() {
+        login("13333333333333", "13333333333333");
     }
 
     @Тогда("врач принимает пациента и создает examinationResult не заполняя поля")
     public void врачПринимаетПациентаИСоздаетExaminationResultНеЗаполняяПоля(){
         webDriver.findElement(By.id("allAppointments")).click();
-        webDriver.findElement(By.id("55555555555555")).click();
+        webDriver.findElement(By.id("15555555555555")).click();
         webDriver.findElement(By.id("acceptPatient")).click();
         webDriver.findElement(By.id("examinationResult")).click();
         webDriver.findElement(By.className("btn-primary")).click();
@@ -47,13 +41,12 @@ public class DoctorAppointment extends Steps{
     @И("выходит ошибка Обязаельное поле.")
     public void выходитОшибкаОбязаельноеПоле(){
         Assertions.assertEquals("Обязательное поле", webDriver.findElement(By.className("alert-warning")).getText());
-        webDriver.close();
     }
 
     @Тогда("врач принимает пациента и создает diagnoseResult не заполняя поля")
     public void врачПринимаетПациентаИСоздаетDiagnoseResultНеЗаполняяПоля(){
         webDriver.findElement(By.id("allAppointments")).click();
-        webDriver.findElement(By.id("55555555555555")).click();
+        webDriver.findElement(By.id("15555555555555")).click();
         webDriver.findElement(By.id("acceptPatient")).click();
         webDriver.findElement(By.id("diagnoseResult")).click();
         webDriver.findElement(By.className("btn-primary")).click();
@@ -62,13 +55,12 @@ public class DoctorAppointment extends Steps{
     @И("выходит ошибка Выберите один из вариантов")
     public void выходитОшибкаВыберитеОдинИзВариантов(){
         Assertions.assertEquals("Выберите один из вариантов", webDriver.findElement(By.className("alert-warning")).getText());
-        webDriver.close();
     }
 
     @Тогда("врач принимает пациента и создает sickList не заполняя поля")
     public void врачПринимаетПациентаИСоздаетSickListНеЗаполняяПоля(){
         webDriver.findElement(By.id("allAppointments")).click();
-        webDriver.findElement(By.id("55555555555555")).click();
+        webDriver.findElement(By.id("15555555555555")).click();
         webDriver.findElement(By.id("acceptPatient")).click();
         webDriver.findElement(By.id("sickList")).click();
         webDriver.findElement(By.className("btn-primary")).click();
@@ -78,13 +70,12 @@ public class DoctorAppointment extends Steps{
     public void выходятОшибки(){
         Assertions.assertEquals("Обязательное поле", webDriver.findElement(By.className("startDate")).getText());
         Assertions.assertEquals("Обязательное поле", webDriver.findElement(By.className("endDate")).getText());
-        Assertions.assertEquals("Требуется ввести 7 цифр", webDriver.findElement(By.className("sickListNumber")).getText());
-        webDriver.close();
+        Assertions.assertEquals("№ состоит только из цифр :", webDriver.findElement(By.className("sickListNumber")).getText());
     }
     @Тогда("врач принимает пациента и создает sickList заполнив только номер SF")
     public void врачПринимаетПациентаИСоздаетSickListЗаполнивТолькоНомерSF(){
         webDriver.findElement(By.id("allAppointments")).click();
-        webDriver.findElement(By.id("55555555555555")).click();
+        webDriver.findElement(By.id("15555555555555")).click();
         webDriver.findElement(By.id("acceptPatient")).click();
         webDriver.findElement(By.id("sickList")).click();
         webDriver.findElement(By.name("number")).sendKeys("wwwwwww");
@@ -96,7 +87,6 @@ public class DoctorAppointment extends Steps{
         Assertions.assertEquals("Обязательное поле", webDriver.findElement(By.className("startDate")).getText());
         Assertions.assertEquals("Обязательное поле", webDriver.findElement(By.className("endDate")).getText());
         Assertions.assertEquals("№ состоит только из цифр : wwwwwww", webDriver.findElement(By.className("sickListNumber")).getText());
-        webDriver.close();
     }
 
 }
--- src/test/java/com/project/crm/ui/steps/DoctorSchedule.java
+++ src/test/java/com/project/crm/ui/steps/DoctorSchedule.java
@@ -1,5 +1,7 @@
 package com.project.crm.ui.steps;
 
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.ru.Допустим;
 import io.cucumber.java.ru.И;
 import io.cucumber.java.ru.Когда;
@@ -10,19 +12,21 @@ import org.openqa.selenium.WebElement;
 
 public class DoctorSchedule extends Steps{
 
-    @Допустим("врач входит на сайт")
-    public void врачВходитНаСайт() {
-        webDriver.get("http://localhost:7777/login");
+    @Before("@doctorSchedule")
+    public void start() {
+        setUp();
     }
 
-    @Когда("врач попадает к себе на страницу")
-    public void врачПопадаетКСебеНаСтраницу(){
-        WebElement username = webDriver.findElement(By.name("username"));
-        username.sendKeys("33333333333333");
-        WebElement password = webDriver.findElement(By.name("password"));
-        password.sendKeys("33333333333333");
-        password.submit();
+    @After("@doctorSchedule")
+    public void finish() {
+        tearDown();
     }
+
+    @Допустим("врач входит на сайт")
+    public void врачВходитНаСайт() {
+        login("13333333333333", "13333333333333");
+    }
+
 
     @Тогда("врач создает рабочий график не заполняя его")
     public void врачСоздаетРабочийГрафикНеЗаполняяЕго(){
@@ -32,6 +36,5 @@ public class DoctorSchedule extends Steps{
     @И("выходит ошибка Обязаельное поле")
     public void выходитОшибкаОбязаельноеПоле(){
         Assertions.assertEquals("Обязательное поле", webDriver.findElement(By.className("alert-warning")).getText());
-        webDriver.close();
     }
 }
