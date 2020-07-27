79d15733b7cee5215cb85571cb6a0269a68feb3d
95shuma <95shuma@gmail.com>
2020-07-27 15:57:17 +0600

    #96 добавил контроллер для вывода врачей

---
+++ src/main/java/com/project/crm/frontend/controller/patient/DoctorsController.java
@@ -0,0 +1,12 @@
+package com.project.crm.frontend.controller.patient;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/patient")
+@AllArgsConstructor
+public class DoctorsController {
+
+}
