2e339ddca0db43c7cbf95ad32ef58b014d03bec6
95shuma <95shuma@gmail.com>
2020-07-27 14:56:47 +0600

    #107 добавил класс AdminAddHospitalSteps

---
+++ src/test/java/com/project/crm/ui/steps/AdminAddHospitalSteps.java
@@ -0,0 +1,15 @@
+package com.project.crm.ui.steps;
+
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+
+public class AdminAddHospitalSteps extends Steps{
+
+    @И("нажимает на кнопку зарегистрировать новое место")
+    public void нажимаетНаКнопкуЗарегистрироватьНовоеМесто() {
+    }
+
+    @Когда("администратор правильно заполняет все поля нажимает сохранить")
+    public void администраторПравильноЗаполняетВсеПоляНажимаетСохранить() {
+    }
+}
