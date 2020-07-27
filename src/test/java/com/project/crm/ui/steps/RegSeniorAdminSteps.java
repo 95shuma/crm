835a889e86de5522d2d14778fc6ac0db8d1eadb9
95shuma <95shuma@gmail.com>
2020-07-27 12:05:18 +0600

    #107 создал класс RegSeniorAdminSteps

---
+++ src/test/java/com/project/crm/ui/steps/RegSeniorAdminSteps.java
@@ -0,0 +1,13 @@
+package com.project.crm.ui.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.ru.Допустим;
+import io.cucumber.java.ru.И;
+import io.cucumber.java.ru.Когда;
+import io.cucumber.java.ru.Тогда;
+
+public class RegSeniorAdminSteps extends Steps{
+
+
+}
