04982fb589319d9bdd825e0d7a8a8f4b7f88fe70
kempl <kempl95.kg@gmail.com>
2020-08-01 00:29:53 +0600

    #127 Поменял WorkSchedule на WeekSchedule. Так как WorkSchedule будет уже рассписанием на определенный день.

--- src/main/java/com/project/crm/backend/model/WeekSchedule.java
+++ src/main/java/com/project/crm/backend/model/WeekSchedule.java
@@ -15,8 +15,8 @@ import java.util.Calendar;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
-@Table(name = "work_schedule")
-public class WorkSchedule {
+@Table(name = "week_schedule")
+public class WeekSchedule {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/project/crm/backend/repository/WorkScheduleRepo.java
+++ src/main/java/com/project/crm/backend/repository/WorkScheduleRepo.java
@@ -1,13 +1,13 @@
 package com.project.crm.backend.repository;
 
-import com.project.crm.backend.model.WorkSchedule;
+import com.project.crm.backend.model.WeekSchedule;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
 
-public interface WorkScheduleRepo extends JpaRepository <WorkSchedule, Long> {
+public interface WorkScheduleRepo extends JpaRepository <WeekSchedule, Long> {
 //    Page<WorkSchedule> findByRegistrationJournalUserInn(Long inn, Pageable pageable);
 //    WorkSchedule findByDateAndRegistrationJournalUserInn(LocalDate date,Long inn);
 }
