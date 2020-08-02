97804f96c280de83f55d0b460a8c11928e6c99d4
kempl <kempl95.kg@gmail.com>
2020-08-03 02:17:21 +0600

    #127 При клике на выбраную должность отправляет запрос на получение врачей, которые работают в том-же ЛПУ что и авторизованный пользователь. Объекты созданы чтобы в последующем отобразить на UI

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,11 +6,46 @@ const baseUrl = 'http://localhost:7777';
 let newLet;
 //--> ============================================================== Переменные ====================================================================
 //--< ============================================================== Объекты ======================================================================
-function Page(id, className, text) {
+function Place(id, name, codePlace, groupCode) {
     this.id = id;
-    this.className = className;
-    this.text = text;
+    this.name = name;
+    this.codePlace = codePlace;
+    this.groupCode = groupCode;
 }
+function Hospital(id, name, Place, address) {
+    this.id = id;
+    this.name = name;
+    this.Place = Place;
+    this.address = address;
+}
+function Position(id, name) {
+    this.id = id;
+    this.name = name;
+}
+function Role(id, name) {
+    this.id = id;
+    this.name = name;
+}
+function User(id, inn, documentNumber, fullName, name, surname, middleName, birthDate, gender, Place) {
+    this.id = id;
+    this.inn = inn;
+    this.documentNumber = documentNumber;
+    this.fullName = fullName;
+    this.name = name;
+    this.surname = surname;
+    this.middleName = middleName;
+    this.birthDate = birthDate;
+    this.gender = gender;
+    this.Place = Place;
+}
+function RegistrationJournal(id, Hospital, User, Position, Role) {
+    this.id = id;
+    this.Hospital = Hospital;
+    this.User = User;
+    this.Position = Position;
+    this.Role = Role;
+}
+
 //--> ============================================================== Объекты ========================================================================
 //--< ============================================================== Разные функции =================================================================
 function saveUser(user) {
@@ -38,8 +73,24 @@ function checkLocalStorageForUserData() {
 }
 //--> ============================================================== Разные функции =================================================================
 //--< ============================================================== Запросы ========================================================================
-
+const fetchDoctorsByPosition = async (idx) => {
+    const getPath = `${baseUrl}/users/positions/${idx}`;
+    const data = await fetch(getPath, {cache: 'no-cache'});
+    return data.json();
+};
+function getDoctorsByPosition(fetch) {
+    fetch.then(data => {
+        //console.log(data);
+        // for (let i = 0; i < data.content.length; i++) {
+        //     let registrationJournal = new RegistrationJournal(data.content[i].id)
+        // }
+    });
+}
 //--> ============================================================== Запросы ========================================================================
 //--< ============================================================== Обновляю ==============================================================
+const selectElement = document.getElementById('positionIdList');
+selectElement.addEventListener('change', (event) => {
+    getDoctorsByPosition(fetchDoctorsByPosition(event.target.value));
+});
 
 //--> ============================================================== Обновляю ==============================================================
