9253a79720cc34e0dbe7a95b3884c0209d8cb882
kempl <kempl95.kg@gmail.com>
2020-08-05 21:29:27 +0600

    #127 заменил js файл с main на отдельный для графика

--- src/main/resources/static/js/newSchedule.js
+++ src/main/resources/static/js/newSchedule.js
@@ -1,11 +1,51 @@
 'use strict';
 //--< ============================================================== Константы ====================================================================
 const baseUrl = 'http://localhost:7777';
+const regUserList = document.getElementById('regUserList');
 //--> ============================================================== Константы ====================================================================
 //--< ============================================================== Переменные ====================================================================
 let newLet;
 //--> ============================================================== Переменные ====================================================================
 //--< ============================================================== Объекты ======================================================================
+function Place(id, name, codePlace, groupCode) {
+    this.id = id;
+    this.name = name;
+    this.codePlace = codePlace;
+    this.groupCode = groupCode;
+}
+function Hospital(id, name, Place, address) {
+    this.id = id;
+    this.name = name;
+    this.Place = Place;
+    this.address = address;
+}
+function Position(id, name) {
+    this.id = id;
+    this.name = name;
+}
+function Role(id, name) {
+    this.id = id;
+    this.name = name;
+}
+function User(id, inn, documentNumber, fullName, name, surname, middleName, birthDate, gender, Place) {
+    this.id = id;
+    this.inn = inn;
+    this.documentNumber = documentNumber;
+    this.fullName = fullName;
+    this.name = name;
+    this.surname = surname;
+    this.middleName = middleName;
+    this.birthDate = birthDate;
+    this.gender = gender;
+    this.Place = Place;
+}
+function RegistrationJournal(id, Hospital, User, Position, Role) {
+    this.id = id;
+    this.Hospital = Hospital;
+    this.User = User;
+    this.Position = Position;
+    this.Role = Role;
+}
 
 //--> ============================================================== Объекты ========================================================================
 //--< ============================================================== Разные функции =================================================================
@@ -34,8 +74,28 @@ function checkLocalStorageForUserData() {
 }
 //--> ============================================================== Разные функции =================================================================
 //--< ============================================================== Запросы ========================================================================
-
+const fetchDoctorsByPosition = async (positionId) => {
+    const getPath = `${baseUrl}/users/positions/${positionId}`;
+    const data = await fetch(getPath, {cache: 'no-cache'});
+    return data.json();
+};
+function getDoctorsByPosition(fetch) {
+    fetch.then(data => {
+        console.log(data);
+        productList.innerHTML = '';
+        for (let i = 0; i<data.content.length; i++){
+            addElement(productList, createProductElement(new Product(data.content[i].id, data.content[i].name, data.content[i].image, data.content[i].qty, data.content[i].description, data.content[i].price)));
+        }
+        // for (let i = 0; i < data.content.length; i++) {
+        //     let registrationJournal = new RegistrationJournal(data.content[i].id)
+        // }
+    });
+}
 //--> ============================================================== Запросы ========================================================================
 //--< ============================================================== Обновляю ==============================================================
+const selectElement = document.getElementById('positionIdList');
+selectElement.addEventListener('change', (event) => {
+    getDoctorsByPosition(fetchDoctorsByPosition(event.target.value));
+});
 
 //--> ============================================================== Обновляю ==============================================================
