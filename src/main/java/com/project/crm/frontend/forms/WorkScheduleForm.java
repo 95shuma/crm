57b0facfad5f21108096b3c8f0f3d8e09fc6c0a6
kempl <kempl95.kg@gmail.com>
2020-08-08 19:55:28 +0600

    #127 Исправил WorkScheduleForm на корректный вариант

--- src/main/java/com/project/crm/frontend/forms/WorkScheduleForm.java
+++ src/main/java/com/project/crm/frontend/forms/WorkScheduleForm.java
@@ -21,4 +21,39 @@ public class WorkScheduleForm {
     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     private LocalTime monday_to;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime tuesday_from;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime tuesday_to;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime wednesday_from;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime wednesday_to;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime thursday_from;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime thursday_to;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime friday_from;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime friday_to;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime saturday_from;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime saturday_to;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime sunday_from;
+
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalTime sunday_to;
 }
