9ad0ee5daf1e613cf751324ce6d06011a5eb61a8
Baktygul <bakulyakozhoshova@gmail.com>
2020-08-09 23:26:12 +0600

    #131 контроллер должности-добавила методы

--- src/main/java/com/project/crm/frontend/controller/admin/PositionController.java
+++ src/main/java/com/project/crm/frontend/controller/admin/PositionController.java
@@ -1,9 +1,11 @@
 package com.project.crm.frontend.controller.admin;
 
 import com.project.crm.backend.dto.PositionDTO;
+import com.project.crm.backend.model.catalog.Position;
 import com.project.crm.backend.services.PositionService;
 import com.project.crm.backend.services.PropertiesService;
 import com.project.crm.backend.services.UserService;
+import com.project.crm.frontend.forms.NewPasswordRegisterForm;
 import com.project.crm.frontend.forms.PositionRegisterForm;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -16,7 +18,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.Optional;
 
+//@RestController
 @Controller
 @RequestMapping("/admin/positions")
 @AllArgsConstructor
@@ -60,4 +64,25 @@ public class PositionController {
         positionService.createPosition(positionRegisterForm);
         return "redirect:/admin/positions";
     }
+
+    @GetMapping("{/positions/update/{position_id}")
+    public String getPositionUpdated(Model model, Principal principal, @PathVariable("position_id") Long position_id){
+        userService.checkUserPresence(model, principal);
+        model.addAttribute("position_id", positionService.getPositionById(position_id));
+        return "/admin/positionController/updatePosition";
+    }
+
+    @PostMapping("/positions/update")
+    public String createPosition2(@Valid Position positionRegisterForm,Long position_id, BindingResult validationResult,
+                                 RedirectAttributes attributes){
+
+        if (validationResult.hasFieldErrors()) {
+            attributes.addFlashAttribute("errors", validationResult.getFieldErrors());
+            return "redirect:/admin/positions/update"+positionService.getPositionById(position_id);
+        }
+
+        positionService.updatePosition(positionRegisterForm);
+        return "redirect:/admin/positions";
+    }
+
 }
