bf6895a17de2aab23268d1e156a30c7110f2a928
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-08-13 21:55:10 +0600

    #140 добавил новые методы findAllHospitalUsers findAllHospitalPatients

--- src/main/java/com/project/crm/backend/repository/RegistrationJournalRepo.java
+++ src/main/java/com/project/crm/backend/repository/RegistrationJournalRepo.java
@@ -1,6 +1,5 @@
 package com.project.crm.backend.repository;
 
-import com.project.crm.backend.model.User;
 import com.project.crm.backend.model.catalog.RegistrationJournal;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,7 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface RegistrationJournalRepo extends JpaRepository<RegistrationJournal, Long> {
     List<RegistrationJournal> findByHospitalId(Long hospitalId);
@@ -17,11 +15,17 @@ public interface RegistrationJournalRepo extends JpaRepository<RegistrationJourn
     RegistrationJournal findByUserInn(Long inn);
     RegistrationJournal findFirstByUserInnAndRoleId(Long inn, Long roleId);
 
+    @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.hospital_id = ?1 order by u.full_name asc", nativeQuery = true)
+    Page<RegistrationJournal> findAllHospitalUsers(Long hospitalId, Pageable pageable);
+
     @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id = 2 order by u.full_name asc", nativeQuery = true)
     Page<RegistrationJournal> findAllSeniorDoctors(Pageable pageable);
 
-    @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id != 5 and rj.role_id != 1  and rj.hospital_id = ?1   order by u.full_name asc", nativeQuery = true)
+    @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id != 5 and rj.role_id != 1  and rj.hospital_id = ?1 order by u.full_name asc", nativeQuery = true)
     Page<RegistrationJournal> findAllHospitalStaff(Long hospitalId, Pageable pageable);
+
+    @Query(value = "select * from users u, registrations_journal rj where u.id = rj.user_id and rj.role_id = 5  and rj.hospital_id = ?1 order by u.full_name asc", nativeQuery = true)
+    Page<RegistrationJournal> findAllHospitalPatients(Long hospitalId, Pageable pageable);
 
     boolean existsByUserInnAndRoleId(Long inn, Long id);
 }
